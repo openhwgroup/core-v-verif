8087f322137c3e3adbd62cb84d91426b1cc61f72
Mike Thompson <mike@openhwgroup.org>
2020-08-06 10:37:12 -0500

    Add source assembly to objdumps
    Signed-off-by: Mike Thompson <mike@openhwgroup.org>

--- cv32/sim/Common.mk
+++ cv32/sim/Common.mk
@@ -243,7 +243,7 @@ sanity: hello-world
 		--change-section-address  .debugger=0x3FC000 \
 		--change-section-address  .debugger_exception=0x3FC400
 	$(RISCV_EXE_PREFIX)readelf -a $< > $*.readelf
-	$(RISCV_EXE_PREFIX)objdump -D $*.elf > $*.objdump
+	$(RISCV_EXE_PREFIX)objdump -D -S $*.elf > $*.objdump
 
 bsp:
 	make -C $(BSP)
