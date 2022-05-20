8e6df86d229fd38bc0f1300488ff0d425b970ea2
Pascal Gouedo <pascal.gouedo@dolphin.fr>
2022-05-20 08:57:38 +0200

    Corrected IMPERAS_TOOLS variable export. Corrected clean and added clean_rtl rules.
    Signed-off-by: Pascal Gouedo <pascal.gouedo@dolphin.fr>

--- mk/uvmt/xrun.mk
+++ mk/uvmt/xrun.mk
@@ -339,7 +339,6 @@ gen_ovpsim_ic:
 	@if [ ! -z "$(CFG_OVPSIM)" ]; then \
 		echo "$(CFG_OVPSIM)" > $(SIM_CFG_RESULTS)/$(TEST_NAME)/$(RUN_INDEX)/ovpsim.ic; \
 	fi
-export IMPERAS_TOOLS=ovpsim.ic
 
 ################################################################################
 # The new general test target
@@ -347,6 +346,7 @@ export IMPERAS_TOOLS=ovpsim.ic
 test: $(XRUN_SIM_PREREQ) hex gen_ovpsim_ic
 	mkdir -p $(SIM_RUN_RESULTS)/test_program && \
 	cd $(SIM_RUN_RESULTS) && \
+	export IMPERAS_TOOLS=$(SIM_CFG_RESULTS)/$(TEST_NAME)/$(RUN_INDEX)/ovpsim.ic && \
 	$(XRUN) \
 		-R -xmlibdirname ../../xcelium.d \
 		-l xrun-$(TEST_NAME).log \
@@ -484,7 +484,7 @@ cov: $(COV_MERGE)
 
 clean:
 	@echo "$(MAKEFILE_LIST)"
-	rm -rf $(SIM_RESULTS)
+	rm -rf $(SIM_RUN_RESULTS)
 
 # Files created by Eclipse when using the Imperas ISS + debugger
 clean_eclipse:
@@ -493,6 +493,8 @@ clean_eclipse:
 	rm  -f stdout.txt
 	rm  -rf workspace
 
-# All generated files plus the clone of the RTL
-clean_all: clean clean_eclipse clean_riscv-dv clean_test_programs clean_bsp clean_compliance clean_embench clean_dpi_dasm_spike clean_svlib
+clean_rtl:
 	rm -rf $(CV_CORE_PKG)
+
+# All generated files plus the clone of the RTL
+clean_all: clean clean_rtl clean_eclipse clean_riscv-dv clean_test_programs clean_bsp clean_compliance clean_embench clean_dpi_dasm_spike clean_svlib
