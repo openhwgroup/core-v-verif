d0070edbe1aa58ea21f9858e6ee8fe3659044f00
Robin Pedersen <Robin.Pedersen@silabs.com>
2022-05-27 14:00:10 +0200

    update 40s core hash
    Signed-off-by: Robin Pedersen <Robin.Pedersen@silabs.com>

--- cv32e40s/sim/ExternalRepos.mk
+++ cv32e40s/sim/ExternalRepos.mk
@@ -15,7 +15,7 @@ export SHELL = /bin/bash
 
 CV_CORE_REPO   ?= https://github.com/openhwgroup/cv32e40s
 CV_CORE_BRANCH ?= master
-CV_CORE_HASH   ?= f776c746e44b7636fe732caa71c0af04e3e118c5
+CV_CORE_HASH   ?= 0e795acee238103e7b6dc0f4d8b7957cc713f019
 CV_CORE_TAG    ?= none
 
 # RISCVDV_REPO    ?= https://github.com/google/riscv-dv
