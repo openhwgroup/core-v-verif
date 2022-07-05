b156843a74fdce32633b7bceebf35648afd7662c
Jean-Roch Coulon <jean-roch.coulon@thalesgroup.com>
2022-07-05 06:45:43 +0200

    Use cv32a6_im_sv0 configuration
    Signed-off-by: Jean-Roch Coulon <jean-roch.coulon@thalesgroup.com>

--- cva6/regress/coremark.sh
+++ cva6/regress/coremark.sh
@@ -24,10 +24,10 @@ if ! [ -n "$DV_SIMULATORS" ]; then
 fi
 
 cd cva6/sim/
-python3 cva6.py --target cv64a6_imafdc_sv39 --iss=$DV_SIMULATORS --iss_yaml=cva6.yaml --c_tests ../tests/custom/coremark/core_main.c --gcc_opts '../tests/custom/coremark/core_list_join.c ../tests/custom/coremark/core_matrix.c ../tests/custom/coremark/core_portme.c ../tests/custom/coremark/core_state.c ../tests/custom/coremark/core_util.c -O3 -fno-tree-loop-distribute-patterns -nostdlib -nostartfiles -mcmodel=medany -DFLAGS_STR="-O3_-fno-tree-loop-distribute-patterns" -DITERATIONS=10 -DPERFORMANCE_RUN -g -nostdlib -nostartfiles ../tests/custom/common/syscalls.c ../tests/custom/common/crt.S -I../tests/custom/env -I../tests/custom/common -lgcc'
-make -C ../../core-v-cores/cva6 clean
-make clean_all
-python3 cva6.py --target cv32a6_imac_sv0 --iss=$DV_SIMULATORS --iss_yaml=cva6.yaml --c_tests ../tests/custom/coremark/core_main.c --gcc_opts '../tests/custom/coremark/core_list_join.c ../tests/custom/coremark/core_matrix.c ../tests/custom/coremark/core_portme.c ../tests/custom/coremark/core_state.c ../tests/custom/coremark/core_util.c -O3 -fno-tree-loop-distribute-patterns -nostdlib -nostartfiles -DFLAGS_STR="-O3_-fno-tree-loop-distribute-patterns" -DITERATIONS=10 -DPERFORMANCE_RUN -g -nostdlib -nostartfiles ../tests/custom/common/syscalls.c ../tests/custom/common/crt.S -I../tests/custom/env -I../tests/custom/common -lgcc'
+# python3 cva6.py --target cv64a6_imafdc_sv39 --iss=$DV_SIMULATORS --iss_yaml=cva6.yaml --c_tests ../tests/custom/coremark/core_main.c --gcc_opts '../tests/custom/coremark/core_list_join.c ../tests/custom/coremark/core_matrix.c ../tests/custom/coremark/core_portme.c ../tests/custom/coremark/core_state.c ../tests/custom/coremark/core_util.c -O3 -fno-tree-loop-distribute-patterns -nostdlib -nostartfiles -mcmodel=medany -DFLAGS_STR="-O3_-fno-tree-loop-distribute-patterns" -DITERATIONS=10 -DPERFORMANCE_RUN -g -nostdlib -nostartfiles ../tests/custom/common/syscalls.c ../tests/custom/common/crt.S -I../tests/custom/env -I../tests/custom/common -lgcc'
+# make -C ../../core-v-cores/cva6 clean
+# make clean_all
+python3 cva6.py --target cv32a6_im_sv0 --iss=$DV_SIMULATORS --iss_yaml=cva6.yaml --c_tests ../tests/custom/coremark/core_main.c --gcc_opts '../tests/custom/coremark/core_list_join.c ../tests/custom/coremark/core_matrix.c ../tests/custom/coremark/core_portme.c ../tests/custom/coremark/core_state.c ../tests/custom/coremark/core_util.c -O3 -funroll-all-loops -fno-tree-loop-distribute-patterns -nostdlib -nostartfiles -DFLAGS_STR="-O3_-fno-tree-loop-distribute-patterns" -DITERATIONS=5 -DPERFORMANCE_RUN -g -nostdlib -nostartfiles ../tests/custom/common/syscalls.c ../tests/custom/common/crt.S -I../tests/custom/env -I../tests/custom/common -lgcc'
 make -C ../../core-v-cores/cva6 clean
 make clean_all
 
