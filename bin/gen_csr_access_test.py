0779e2a78f013e6fa9672a9e95b31aaddc6509fc
Henrik Fegran <Henrik.Fegran@silabs.com>
2022-08-22 11:05:29 +0200

    WiP csr_script
    Signed-off-by: Henrik Fegran <Henrik.Fegran@silabs.com>

--- bin/gen_csr_access_test.py
+++ bin/gen_csr_access_test.py
@@ -9,6 +9,8 @@ if (sys.version_info < (3,0,0)):
     print ('Requires python 3')
     exit(1)
 
+supported_cores = ["cv32e40s", "cv32e40x"]
+
 try:
     default_core = os.environ['CV_CORE']
 except KeyError:
@@ -16,30 +18,41 @@ except KeyError:
 
 topdir = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..'))
 parser = argparse.ArgumentParser()
-parser.add_argument("--core",            help="Set the core to generate test for", choices=['cv32e40s', 'cv32e40x'])
-parser.add_argument("--pmp_num_regions", help="Set number of Pmp regions",         choices=['0', '64'])
-parser.add_argument("--clic_enable",     help="Enable clic support",               action="store_true")
-parser.add_argument("--output",          help="Output path",                       type=str)
-parser.add_argument("--iterations",      help="Number of generated tests",         type=str)
+parser.add_argument("--dry_run",                help="Prints generated yaml to stdout",   action="store_true")
+parser.add_argument("--core",                   help="Set the core to generate test for", choices=supported_cores)
+parser.add_argument("--pmp_num_regions",        help="Set number of Pmp regions",         type=str, default="0")
+parser.add_argument("--mhpmcounter_num",        help="Set number of mhpmcounters",        type=str, default="3")
+parser.add_argument("--clic_enable",            help="Enable clic support",               action="store_true", default=False)
+parser.add_argument("--zc_enable",              help="Enable Zc support",                 action="store_true", default=False)
+parser.add_argument("--clint_enable",           help="Enable Basic Interrupts",           action="store_true", default=False)
+parser.add_argument("--output",                 help="Output path",                       type=str)
+parser.add_argument("--iterations",             help="Number of generated tests",         type=str, default="1")
 
 args = parser.parse_args()
 
-# TODO fix
-if (args.output == None or args.core == None):
+if (args.core == None or args.core not in supported_cores):
+    print("Error: No core supported core specified")
+    exit(1)
+if (args.output == None):
     print("Error: no output path")
     exit(1)
+if int(args.pmp_num_regions) not in range(65):
+    print("Error: unsupported number of regions setting")
+    exit(1)
 
-print('{}'.format(args))
-script_path   = os.path.join(topdir, '{}/vendor_lib/google/riscv-dv/scripts/gen_csr_test.py'.format(args.core.lower()))
-file_path     = os.path.join(topdir, '{}'.format(args.core.lower()) + '/env/corev-dv/{}'.format(args.core.lower()) + '_csr_template.yaml')
-template_path = os.path.join(topdir, './bin/templates/csr_access_test_template.S')
+if args.dry_run:
+    print('{}'.format(args))
 
-def run_riscv_dv_gen_csr_script():
+script_path      = os.path.join(topdir, '{}/vendor_lib/google/riscv-dv/scripts/gen_csr_test.py'.format(args.core.lower()))
+yaml_file_path   = os.path.join(topdir, '{}'.format(args.core.lower()) + '/env/corev-dv/{}'.format(args.core.lower()) + '_csr_template.yaml')
+template_path    = os.path.join(topdir, './bin/templates/csr_access_test_template.S')
+output_yaml_path = ""
+
+def run_riscv_dv_gen_csr_script(output_yaml_path):
     try:
-        print(os.path.join(topdir, '{}/vendor_lib/google/riscv-dv/scripts/gen_csr_test.py'.format(args.core.lower())))
-        print("{}".format(script_path))
-        print("{}".format(file_path))
-        script_args = { "csr_file"   : ' --csr_file {}'.format(file_path),
+        print("riscv-dv script path: {}".format(script_path))
+        print("csr description used: {}".format(output_yaml_path))
+        script_args = { "csr_file"   : ' --csr_file {}'.format(output_yaml_path),
                         "xlen"       : ' --xlen 32',
                         "out"        : ' --out ' + args.output,
                         "iterations" : ' --iterations ' + args.iterations}
@@ -47,7 +60,67 @@ def run_riscv_dv_gen_csr_script():
     except Exception as e:
         print(e)
 
-def gen_riscv_dv_gen_csr_file(iteration = 0):
+def preprocess_yaml():
+    input_script_path = yaml_file_path
+    w_enable = True
+    w_enable_n = w_enable
+    str_args = ""
+    enabled_features = {"clic": False, "clint": False, "zc": False, "pmp": 0, "mhpmcounters": 0}
+
+    if (args.clic_enable):
+        str_args = str_args + "_clic"
+        enabled_features["clic"] = True
+    if (args.clint_enable or not args.clic_enable):
+        str_args = str_args + "_clint"
+        enabled_features["clint"] = True if not enabled_features["clic"] else False
+    if (args.zc_enable):
+        str_args = str_args + "_zc"
+        enabled_features["zc"] = True
+    if (int(args.pmp_num_regions) > 0):
+        str_args = str_args + "_pmp" + args.pmp_num_regions
+        enabled_features["pmp"] = int(args.pmp_num_regions)
+    if (int(args.mhpmcounter_num) > 0):
+        str_args = str_args + "_mhpmctr" + args.mhpmcounter_num
+        enabled_features["mhpmcounters"] = int(args.mhpmcounter_num)
+
+    output_script_path = os.path.join(args.output) + "{}_csr_template.yaml".format(args.core.lower() + str_args)
+    if not args.dry_run:
+        output_script_handle = open(output_script_path, "w")
+
+    input_script_handle = open(input_script_path, "r")
+    input_script_lines = input_script_handle.readlines()
+    for line in input_script_lines:
+        input_list = line.split()
+        # Cond <true/false>
+        if len(input_list) == 3 and input_list[0] == "###" and input_list[1].lower() == "cond":
+            if not enabled_features[input_list[2].lower()]:
+                w_enable   = False
+                w_enable_n = False
+        # Cond <key pair>
+        elif len(input_list) == 4 and input_list[0] == "###" and input_list[1].lower() == "cond":
+            if (int(input_list[3].lower()) > int(enabled_features[input_list[2].lower()])):
+                w_enable   = False
+                w_enable_n = False
+            else:
+                w_enable_n = True
+                w_enable   = True
+        # Endcond
+        elif len(input_list) == 2 and input_list[0] == "###" and input_list[1].lower() == "endcond":
+            w_enable_n = True
+        if w_enable == True:
+            if args.dry_run:
+                print(line.strip("\n"))
+            else:
+                output_script_handle.write(line)
+        if w_enable == False:
+            w_enable = w_enable_n
+
+    if not args.dry_run:
+        output_script_handle.close()
+    input_script_handle.close()
+    return output_script_path
+
+def gen_riscv_dv_gen_csr_file(iteration = 1):
     try:
         template_handle = open(template_path, "r")
         tlines = template_handle.readlines()
@@ -83,6 +156,9 @@ def gen_riscv_dv_gen_csr_file(iteration = 0):
     except Exception as e:
         print(e)
 
-run_riscv_dv_gen_csr_script()
-for iteration in range(int(args.iterations)):
-    gen_riscv_dv_gen_csr_file(iteration)
+if args.dry_run:
+    preprocess_yaml()
+else:
+    run_riscv_dv_gen_csr_script(preprocess_yaml())
+    for iteration in range(int(args.iterations)):
+        gen_riscv_dv_gen_csr_file(iteration)
