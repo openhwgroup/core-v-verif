5e43da5a0f143eb1aa9c7cd3c078b6186f096561
Marton Teilgard <mateilga@silabs.com>
2022-08-23 10:48:56 +0200

    "Fixed support logic reset and zc assert logic"
    Signed-off-by: Marton Teilgard <mateilga@silabs.com>

--- cv32e40s/tb/uvmt/uvmt_cv32e40s_support_logic.sv
+++ cv32e40s/tb/uvmt/uvmt_cv32e40s_support_logic.sv
@@ -37,6 +37,8 @@ module uvmt_cv32e40s_support_logic
     // in other words a subop has triggered an exception. WB stage timing.
     logic exception_active;
 
+    // Signal indicates data bus address phase completed last cycle
+    logic data_bus_gnt_q;
 
 
     // ---------------------------------------------------------------------------
@@ -44,15 +46,20 @@ module uvmt_cv32e40s_support_logic
     // ---------------------------------------------------------------------------
 
 
-    // Check if an obi data req arrives after an exception is triggered.
+    // Check if a new obi data req arrives after an exception is triggered.
     // Used to verify exception timing with multiop instruction
     always @(posedge support_if.clk_i or negedge support_if.rst_ni) begin
-      if (support_if.rst_ni) begin
+      if (!support_if.rst_ni) begin
         support_if.req_after_exception_o <= 0;
         exception_active <= 0;
+        data_bus_gnt_q <= 0;
       end else  begin
+        // set prev bus gnt
+        data_bus_gnt_q <= support_if.data_bus_gnt_i;
+
+        // is a trap taken in WB?
         if (support_if.ctrl_fsm_o_i.pc_set && (support_if.ctrl_fsm_o_i.pc_mux == PC_TRAP_DBE || support_if.ctrl_fsm_o_i.pc_mux == PC_TRAP_EXC)) begin
-          if (support_if.data_bus_req_i) begin
+          if (support_if.data_bus_req_i && data_bus_gnt_q) begin
             support_if.req_after_exception_o <= 1;
           end
           exception_active <= 1;
@@ -60,9 +67,11 @@ module uvmt_cv32e40s_support_logic
           exception_active <= 0;
           support_if.req_after_exception_o <= 0;
 
-        end else if (exception_active && support_if.data_bus_req_i) begin
+        end else if (exception_active && data_bus_gnt_q && support_if.data_bus_req_i) begin
           support_if.req_after_exception_o <= 1;
         end
+
+
 
       end
 
