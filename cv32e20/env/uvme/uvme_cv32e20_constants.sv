a05dd6adc0c1f75fde0301b792122208c433870c
Mike Thompson <mike@openhwgroup.org>
2022-10-12 10:18:55 -0400

    Set core clock period to 10ns
    Signed-off-by: Mike Thompson <mike@openhwgroup.org>

--- cv32e20/env/uvme/uvme_cv32e20_constants.sv
+++ cv32e20/env/uvme/uvme_cv32e20_constants.sv
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
 // Copyright 2020,2022 OpenHW Group
 // Copyright 2020 Datum Technology Corporation
-// 
+//
 // Licensed under the Solderpad Hardware Licence, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     https://solderpad.org/licenses/
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,8 +19,8 @@
 `define __UVME_CV32E20_CONSTANTS_SV__
 
 
-parameter uvme_cv32e20_sys_default_clk_period   =  1_500; // 10ns
-parameter uvme_cv32e20_debug_default_clk_period = 10_000; // 10ns
+parameter uvme_cv32e20_sys_default_clk_period   =  5_000; // 10ns (it's actually a half period)
+parameter uvme_cv32e20_debug_default_clk_period = 10_000; // 20ns
 
 
 `endif // __UVME_CV32E20_CONSTANTS_SV__
