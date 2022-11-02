a301ecb45f1dde545d9cf887029996a3464630b1
Henrik Fegran <Henrik.Fegran@silabs.com>
2022-11-02 15:55:20 +0100

    Added missing stdint.h includes
    Signed-off-by: Henrik Fegran <Henrik.Fegran@silabs.com>

--- cv32e40s/tests/programs/custom/misalign/misalign.c
+++ cv32e40s/tests/programs/custom/misalign/misalign.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <strings.h>
 
 //#include "../../../core/custom/startup/support.h"
--- cv32e40s/tests/programs/custom/xsecure_csrs/xsecure_csrs.c
+++ cv32e40s/tests/programs/custom/xsecure_csrs/xsecure_csrs.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 #define CPUADDR_CPUCTRL     0xBF0
 #define CPUADDR_SECURESEED0 0xBF9
