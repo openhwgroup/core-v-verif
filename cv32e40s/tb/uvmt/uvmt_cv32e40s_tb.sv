997f484def97d7059a9f5c7054b70a5393ea1b01
Robin Pedersen <Robin.Pedersen@silabs.com>
2022-11-02 11:44:46 +0100

    rvfi_assert: fix bind issue in sim
    Signed-off-by: Robin Pedersen <Robin.Pedersen@silabs.com>

--- cv32e40s/tb/uvmt/uvmt_cv32e40s_tb.sv
+++ cv32e40s/tb/uvmt/uvmt_cv32e40s_tb.sv
@@ -652,7 +652,7 @@ module uvmt_cv32e40s_tb;
 
   // RVFI assertions
 
-  bind  cv32e40s_wrapper.rvfi_i
+  bind  dut_wrap.cv32e40s_wrapper_i.rvfi_i
     uvmt_cv32e40s_rvfi_assert  rvfi_assert_i (
       .*
     );
