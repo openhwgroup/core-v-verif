94879a6b6e366b60f71d32c183b619760d569a93
Yoann Pruvost <yoann.pruvost@dolphin.fr>
2023-03-13 12:00:00 +0800

    Adding imperasDV coverage to e40p testbench

--- cv32e40p/tb/uvmt/uvmt_cv32e40p_imperas_dv_wrap.sv
+++ cv32e40p/tb/uvmt/uvmt_cv32e40p_imperas_dv_wrap.sv
@@ -246,140 +246,143 @@ module uvmt_cv32e40p_imperas_dv_wrap
   import uvmt_cv32e40p_pkg::*;
   import rvviApiPkg::*;
   #(
-   )
+    )
 
-   (
-           rvviTrace  rvvi // RVVI SystemVerilog Interface
-   );
+    (
+        rvviTrace  rvvi // RVVI SystemVerilog Interface
+    );
 
-   trace2api       #(.CMP_PC      (1),
-                   .CMP_INS     (1),
-                   .CMP_GPR     (1),
-                   .CMP_FPR     (0),
-                   .CMP_VR      (0),
-                   .CMP_CSR     (1)
-                   )
-                   trace2api(rvvi);
+    trace2api #(
+        .CMP_PC      (1),
+        .CMP_INS     (1),
+        .CMP_GPR     (1),
+        .CMP_FPR     (0),
+        .CMP_VR      (0),
+        .CMP_CSR     (1)
+    )
+    trace2api(rvvi);
 
-   trace2log       trace2log(rvvi);
+    trace2log       idv_trace2log(rvvi);
+    trace2cov       idv_trace2cov(rvvi);
 
-   string info_tag = "ImperasDV_wrap";
+    string info_tag = "ImperasDV_wrap";
 
-   ////////////////////////////////////////////////////////////////////////////
-   // Adopted from:
-   // ImperasDV/examples/openhwgroup_cv32e40x/systemverilog/cv32e40x_testbench.sv
-   //
-   // InstrunctionBusFault(48) is in fact a TRAP which is derived externally
-   // This is strange as other program TRAPS are derived by the model, for now
-   // We have to ensure we do not step the REF model for this TRAP as it will
-   // Step too far. So instead we block it as being VALID, but pass on the
-   // signals.
-   // maybe we need a different way to communicate this to the model, for
-   // instance the ability to register a callback on fetch, in order to assert
-   // this signal.
-   ////////////////////////////////////////////////////////////////////////////
-   assign rvvi.clk            = `RVFI_IF.clk_i;
-   assign rvvi.valid[0][0]    = `RVFI_IF.rvfi_valid;
-   assign rvvi.order[0][0]    = `RVFI_IF.rvfi_order;
-   assign rvvi.insn[0][0]     = `RVFI_IF.rvfi_insn;
-   assign rvvi.trap[0][0]     = `RVFI_IF.rvfi_trap.trap;
-   assign rvvi.intr[0][0]     = `RVFI_IF.rvfi_intr;
-   assign rvvi.mode[0][0]     = `RVFI_IF.rvfi_mode;
-   assign rvvi.ixl[0][0]      = `RVFI_IF.rvfi_ixl;
-   assign rvvi.pc_rdata[0][0] = `RVFI_IF.rvfi_pc_rdata;
-  //  assign rvvi.pc_wdata[0][0] = `RVFI_IF.rvfi_pc_wdata;
+    ////////////////////////////////////////////////////////////////////////////
+    // Adopted from:
+    // ImperasDV/examples/openhwgroup_cv32e40x/systemverilog/cv32e40x_testbench.sv
+    //
+    // InstrunctionBusFault(48) is in fact a TRAP which is derived externally
+    // This is strange as other program TRAPS are derived by the model, for now
+    // We have to ensure we do not step the REF model for this TRAP as it will
+    // Step too far. So instead we block it as being VALID, but pass on the
+    // signals.
+    // maybe we need a different way to communicate this to the model, for
+    // instance the ability to register a callback on fetch, in order to assert
+    // this signal.
+    ////////////////////////////////////////////////////////////////////////////
+    assign rvvi.clk            = `RVFI_IF.clk_i;
+    assign rvvi.valid[0][0]    = `RVFI_IF.rvfi_valid;
+    assign rvvi.order[0][0]    = `RVFI_IF.rvfi_order;
+    assign rvvi.insn[0][0]     = `RVFI_IF.rvfi_insn;
+    assign rvvi.trap[0][0]     = `RVFI_IF.rvfi_trap.trap;
+    assign rvvi.intr[0][0]     = `RVFI_IF.rvfi_intr;
+    assign rvvi.mode[0][0]     = `RVFI_IF.rvfi_mode;
+    assign rvvi.ixl[0][0]      = `RVFI_IF.rvfi_ixl;
+    assign rvvi.pc_rdata[0][0] = `RVFI_IF.rvfi_pc_rdata;
+    //  assign rvvi.pc_wdata[0][0] = `RVFI_IF.rvfi_pc_wdata;
 
-   `RVVI_SET_CSR( `CSR_MSTATUS_ADDR,       mstatus       )
-   `RVVI_SET_CSR( `CSR_MISA_ADDR,          misa          )
-   `RVVI_SET_CSR( `CSR_MIE_ADDR,           mie           )
-   `RVVI_SET_CSR( `CSR_MTVEC_ADDR,         mtvec         )
-   `RVVI_SET_CSR( `CSR_MCOUNTINHIBIT_ADDR, mcountinhibit )
-   `RVVI_SET_CSR( `CSR_MSCRATCH_ADDR,      mscratch      )
-   `RVVI_SET_CSR( `CSR_MEPC_ADDR,          mepc          )
-   `RVVI_SET_CSR( `CSR_MCAUSE_ADDR,        mcause        )
-  //  `RVVI_SET_CSR( `CSR_MTVAL_ADDR,         mtval         )
-   `RVVI_SET_CSR( `CSR_MIP_ADDR,           mip           )
-  //  `RVVI_SET_CSR( `CSR_MCYCLE_ADDR,        mcycle        )
-   `RVVI_SET_CSR( `CSR_MINSTRET_ADDR,      minstret      )
-  //  `RVVI_SET_CSR( `CSR_MCYCLEH_ADDR,       mcycleh       )
-  //  `RVVI_SET_CSR( `CSR_MINSTRETH_ADDR,     minstreth     )
-   `RVVI_SET_CSR( `CSR_MVENDORID_ADDR,     mvendorid     )
-   `RVVI_SET_CSR( `CSR_MARCHID_ADDR,       marchid       )
-  //  `RVVI_SET_CSR( `CSR_MIMPID_ADDR,        mimpid        )
-   `RVVI_SET_CSR( `CSR_MHARTID_ADDR,       mhartid       )
+    `RVVI_SET_CSR( `CSR_MSTATUS_ADDR,       mstatus       )
+    `RVVI_SET_CSR( `CSR_MISA_ADDR,          misa          )
+    `RVVI_SET_CSR( `CSR_MIE_ADDR,           mie           )
+    `RVVI_SET_CSR( `CSR_MTVEC_ADDR,         mtvec         )
+    `RVVI_SET_CSR( `CSR_MCOUNTINHIBIT_ADDR, mcountinhibit )
+    `RVVI_SET_CSR( `CSR_MSCRATCH_ADDR,      mscratch      )
+    `RVVI_SET_CSR( `CSR_MEPC_ADDR,          mepc          )
+    `RVVI_SET_CSR( `CSR_MCAUSE_ADDR,        mcause        )
+    //  `RVVI_SET_CSR( `CSR_MTVAL_ADDR,         mtval         )
+    `RVVI_SET_CSR( `CSR_MIP_ADDR,           mip           )
+    //  `RVVI_SET_CSR( `CSR_MCYCLE_ADDR,        mcycle        )
+    `RVVI_SET_CSR( `CSR_MINSTRET_ADDR,      minstret      )
+    //  `RVVI_SET_CSR( `CSR_MCYCLEH_ADDR,       mcycleh       )
+    //  `RVVI_SET_CSR( `CSR_MINSTRETH_ADDR,     minstreth     )
+    `RVVI_SET_CSR( `CSR_MVENDORID_ADDR,     mvendorid     )
+    `RVVI_SET_CSR( `CSR_MARCHID_ADDR,       marchid       )
+    //  `RVVI_SET_CSR( `CSR_MIMPID_ADDR,        mimpid        )
+    `RVVI_SET_CSR( `CSR_MHARTID_ADDR,       mhartid       )
 
-  //  `RVVI_SET_CSR( `CSR_TSELECT_ADDR,       tselect       )
-   `RVVI_SET_CSR( `CSR_DCSR_ADDR,          dcsr          )
-   `RVVI_SET_CSR( `CSR_DPC_ADDR,           dpc           )
-   `RVVI_SET_CSR_VEC(`CSR_DSCRATCH0_ADDR, dscratch, 0)
-   `RVVI_SET_CSR_VEC(`CSR_DSCRATCH1_ADDR, dscratch, 1)
-   `RVVI_SET_CSR_VEC(`CSR_TDATA1_ADDR, tdata, 1)
-   `RVVI_SET_CSR_VEC(`CSR_TDATA2_ADDR, tdata, 2)
-   `RVVI_SET_CSR( `CSR_TINFO_ADDR,         tinfo         )
+    //  `RVVI_SET_CSR( `CSR_TSELECT_ADDR,       tselect       )
+    `RVVI_SET_CSR( `CSR_DCSR_ADDR,          dcsr          )
+    `RVVI_SET_CSR( `CSR_DPC_ADDR,           dpc           )
+    `RVVI_SET_CSR_VEC(`CSR_DSCRATCH0_ADDR, dscratch, 0)
+    `RVVI_SET_CSR_VEC(`CSR_DSCRATCH1_ADDR, dscratch, 1)
+    `RVVI_SET_CSR_VEC(`CSR_TDATA1_ADDR, tdata, 1)
+    `RVVI_SET_CSR_VEC(`CSR_TDATA2_ADDR, tdata, 2)
+    `RVVI_SET_CSR( `CSR_TINFO_ADDR,         tinfo         )
 
-   ////////////////////////////////////////////////////////////////////////////
-   // Assign the RVVI GPR registers
-   ////////////////////////////////////////////////////////////////////////////
-   bit [31:0] XREG[32];
-   genvar gi;
-   generate
-       for(gi=0; gi<32; gi++)
-           assign rvvi.x_wdata[0][0][gi] = XREG[gi];
-   endgenerate
+    ////////////////////////////////////////////////////////////////////////////
+    // Assign the RVVI GPR registers
+    ////////////////////////////////////////////////////////////////////////////
+    bit [31:0] XREG[32];
+    genvar gi;
+    generate
+        for(gi=0; gi<32; gi++) begin
+            assign rvvi.x_wdata[0][0][gi] = XREG[gi];
+        end
+    endgenerate
 
-   always @(*) begin
-       int i;
-       for (i=1; i<32; i++) begin
-           XREG[i] = 32'b0;
-           // TODO: This current RVFI implementation will only allow a single destination
-           //       register to be written. For some instructions this is not sufficient
-           //       This code will need enhancing once the RVFI has the ability to describe
-           //       multiple target register writes, for a single instruction
-           if (`RVFI_IF.rvfi_rd_addr==5'(i))
-               XREG[i] = `RVFI_IF.rvfi_rd_wdata;
-       end
-   end
+    always @(*) begin
+        int i;
+        for (i=1; i<32; i++) begin
+            XREG[i] = 32'b0;
+            // TODO: This current RVFI implementation will only allow a single destination
+            //       register to be written. For some instructions this is not sufficient
+            //       This code will need enhancing once the RVFI has the ability to describe
+            //       multiple target register writes, for a single instruction
+            if (`RVFI_IF.rvfi_rd_addr==5'(i))
+            XREG[i] = `RVFI_IF.rvfi_rd_wdata;
+        end
+    end
 
-   assign rvvi.x_wb[0][0] = 1 << `RVFI_IF.rvfi_rd_addr; // TODO: originally rvfi_rd_addr
+    assign rvvi.x_wb[0][0] = 1 << `RVFI_IF.rvfi_rd_addr; // TODO: originally rvfi_rd_addr
 
-   ////////////////////////////////////////////////////////////////////////////
-   // DEBUG REQUESTS,
-   ////////////////////////////////////////////////////////////////////////////
-   logic debug_req_i;
-   assign debug_req_i = `DUT_PATH.debug_req_i;
-   always @(debug_req_i) begin
-       void'(rvvi.net_push("haltreq", debug_req_i));
-   end
+    ////////////////////////////////////////////////////////////////////////////
+    // DEBUG REQUESTS,
+    ////////////////////////////////////////////////////////////////////////////
+    logic debug_req_i;
+    assign debug_req_i = `DUT_PATH.debug_req_i;
+    always @(debug_req_i) begin
+        void'(rvvi.net_push("haltreq", debug_req_i));
+    end
 
-   ////////////////////////////////////////////////////////////////////////////
-   // INTERRUPTS
-   // assert when MIP or cause bit
-   // negate when posedge clk && valid=1 && debug=0
-   ////////////////////////////////////////////////////////////////////////////
-  `RVVI_WRITE_IRQ(MSWInterrupt,        3)
-  `RVVI_WRITE_IRQ(MTimerInterrupt,     7)
-  `RVVI_WRITE_IRQ(MExternalInterrupt, 11)
-  `RVVI_WRITE_IRQ(LocalInterrupt0,    16)
-  `RVVI_WRITE_IRQ(LocalInterrupt1,    17)
-  `RVVI_WRITE_IRQ(LocalInterrupt2,    18)
-  `RVVI_WRITE_IRQ(LocalInterrupt3,    19)
-  `RVVI_WRITE_IRQ(LocalInterrupt4,    20)
-  `RVVI_WRITE_IRQ(LocalInterrupt5,    21)
-  `RVVI_WRITE_IRQ(LocalInterrupt6,    22)
-  `RVVI_WRITE_IRQ(LocalInterrupt7,    23)
-  `RVVI_WRITE_IRQ(LocalInterrupt8,    24)
-  `RVVI_WRITE_IRQ(LocalInterrupt9,    25)
-  `RVVI_WRITE_IRQ(LocalInterrupt10,   26)
-  `RVVI_WRITE_IRQ(LocalInterrupt11,   27)
-  `RVVI_WRITE_IRQ(LocalInterrupt12,   28)
-  `RVVI_WRITE_IRQ(LocalInterrupt13,   29)
-  `RVVI_WRITE_IRQ(LocalInterrupt14,   30)
-  `RVVI_WRITE_IRQ(LocalInterrupt15,   31)
+    ////////////////////////////////////////////////////////////////////////////
+    // INTERRUPTS
+    // assert when MIP or cause bit
+    // negate when posedge clk && valid=1 && debug=0
+    ////////////////////////////////////////////////////////////////////////////
+    `RVVI_WRITE_IRQ(MSWInterrupt,        3)
+    `RVVI_WRITE_IRQ(MTimerInterrupt,     7)
+    `RVVI_WRITE_IRQ(MExternalInterrupt, 11)
+    `RVVI_WRITE_IRQ(LocalInterrupt0,    16)
+    `RVVI_WRITE_IRQ(LocalInterrupt1,    17)
+    `RVVI_WRITE_IRQ(LocalInterrupt2,    18)
+    `RVVI_WRITE_IRQ(LocalInterrupt3,    19)
+    `RVVI_WRITE_IRQ(LocalInterrupt4,    20)
+    `RVVI_WRITE_IRQ(LocalInterrupt5,    21)
+    `RVVI_WRITE_IRQ(LocalInterrupt6,    22)
+    `RVVI_WRITE_IRQ(LocalInterrupt7,    23)
+    `RVVI_WRITE_IRQ(LocalInterrupt8,    24)
+    `RVVI_WRITE_IRQ(LocalInterrupt9,    25)
+    `RVVI_WRITE_IRQ(LocalInterrupt10,   26)
+    `RVVI_WRITE_IRQ(LocalInterrupt11,   27)
+    `RVVI_WRITE_IRQ(LocalInterrupt12,   28)
+    `RVVI_WRITE_IRQ(LocalInterrupt13,   29)
+    `RVVI_WRITE_IRQ(LocalInterrupt14,   30)
+    `RVVI_WRITE_IRQ(LocalInterrupt15,   31)
 
-  /////////////////////////////////////////////////////////////////////////////
-  // REF control
-  /////////////////////////////////////////////////////////////////////////////
-  task ref_init;
+    /////////////////////////////////////////////////////////////////////////////
+    // REF control
+    /////////////////////////////////////////////////////////////////////////////
+    task ref_init;
     string test_program_elf;
     reg [31:0] hart_id;
 
@@ -393,23 +396,23 @@ module uvmt_cv32e40p_imperas_dv_wrap
     // Initialize REF and load the test-program into it's memory (do this before initializing the DUT).
     // TODO: is this the best place for this?
     if (!rvviVersionCheck(RVVI_API_VERSION)) begin
-      `uvm_fatal(info_tag, $sformatf("Expecting RVVI API version %0d.", RVVI_API_VERSION))
+        `uvm_fatal(info_tag, $sformatf("Expecting RVVI API version %0d.", RVVI_API_VERSION))
     end
     // Test-program must have been compiled before we got here...
     if ($value$plusargs("elf_file=%s", test_program_elf)) begin
-      `uvm_info(info_tag, $sformatf("ImperasDV loading test_program %0s", test_program_elf), UVM_NONE)
-      void'(rvviRefConfigSetString(IDV_CONFIG_MODEL_VENDOR,  "openhwgroup.ovpworld.org"));
-      void'(rvviRefConfigSetString(IDV_CONFIG_MODEL_NAME,    "CVE4P"));
-      void'(rvviRefConfigSetString(IDV_CONFIG_MODEL_VARIANT, "CV32E40P"));
-      if (!rvviRefInit(test_program_elf)) begin
-        `uvm_fatal(info_tag, "rvviRefInit failed")
-      end
-      else begin
-        `uvm_info(info_tag, "rvviRefInit() succeed", UVM_NONE)
-      end
+        `uvm_info(info_tag, $sformatf("ImperasDV loading test_program %0s", test_program_elf), UVM_NONE)
+        void'(rvviRefConfigSetString(IDV_CONFIG_MODEL_VENDOR,  "openhwgroup.ovpworld.org"));
+        void'(rvviRefConfigSetString(IDV_CONFIG_MODEL_NAME,    "CVE4P"));
+        void'(rvviRefConfigSetString(IDV_CONFIG_MODEL_VARIANT, "CV32E40P"));
+        if (!rvviRefInit(test_program_elf)) begin
+            `uvm_fatal(info_tag, "rvviRefInit failed")
+        end
+        else begin
+            `uvm_info(info_tag, "rvviRefInit() succeed", UVM_NONE)
+        end
     end
     else begin
-      `uvm_fatal(info_tag, "No test_program specified")
+        `uvm_fatal(info_tag, "No test_program specified")
     end
 
     hart_id = 32'h0000_0000;
