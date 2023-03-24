440c33b16209e7ea5ae5669120fcde488f25607a
Halfdan Bechmann Dolva <halfdan.bechmann@silabs.com>
2023-03-24 11:32:49 +0100

    Fixed license header
    Signed-off-by: Halfdan Bechmann Dolva <halfdan.bechmann@silabs.com>

--- cv32e40s/tests/programs/custom/debug_test_0_triggers/debug_test_0_trigger.c
+++ cv32e40s/tests/programs/custom/debug_test_0_triggers/debug_test_0_trigger.c
@@ -1,23 +1,29 @@
-/*
-**
-** Copyright 2023 OpenHW Group
-**
-** Licensed under the Solderpad Hardware Licence, Version 2.0 (the "License");
-** you may not use this file except in compliance with the License.
-** You may obtain a copy of the License at
-**
-**     https://solderpad.org/licenses/
-**
-** Unless required by applicable law or agreed to in writing, software
-** distributed under the License is distributed on an "AS IS" BASIS,
-** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-** See the License for the specific language governing permissions and
-** limitations under the License.
-**
-*******************************************************************************
-** Debug 0 triggers test
-*******************************************************************************
-*/
+// Copyright 2023 Silicon Labs, Inc.
+//
+// This file, and derivatives thereof are licensed under the
+// Solderpad License, Version 2.0 (the "License");
+// Use of this file means you agree to the terms and conditions
+// of the license and are in full compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://solderpad.org/licenses/SHL-2.0/
+//
+// Unless required by applicable law or agreed to in writing, software
+// and hardware implementations thereof
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESSED OR IMPLIED.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+////////////////////////////////////////////////////////////////////////////////
+// Author: Halfdan Bechmann - halfdan.bechmann@silabs.com                     //
+//                                                                            //
+// Debug 0 Triggers Test                                                      //
+//                                                                            //
+// Requires: DBG_NUM_TRIG == 0                                                //
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
+
 
 #include <stdio.h>
 #include <stdint.h>
