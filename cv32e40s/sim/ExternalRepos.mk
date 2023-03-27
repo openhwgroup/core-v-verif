cb71f74d889f8b5e0eacddc3f54216d006c58a16
Henrik Fegran <Henrik.Fegran@silabs.com>
2023-03-27 16:24:48 +0200

    Updated riscv-dv hash: reserved a0 due to cm.popretz
    Signed-off-by: Henrik Fegran <Henrik.Fegran@silabs.com>

--- cv32e40s/sim/ExternalRepos.mk
+++ cv32e40s/sim/ExternalRepos.mk
@@ -23,7 +23,7 @@ RISCVDV_REPO    ?= https://github.com/silabs-hfegran/riscv-dv
 #RISCVDV_BRANCH  ?= master
 RISCVDV_BRANCH  ?= dev_hf_rvdv_clic_support_270323
 #RISCVDV_HASH    ?= 797aa0762de8e2c8d427b978483b47dd7649954a
-RISCVDV_HASH    ?= 06cac944fc168d4671eb75a5b90dc2d782f9faa1
+RISCVDV_HASH    ?= 7d98c84978a09031c1013ce66c9fa8d283ef02c6
 
 EMBENCH_REPO    ?= https://github.com/embench/embench-iot.git
 EMBENCH_BRANCH  ?= master
