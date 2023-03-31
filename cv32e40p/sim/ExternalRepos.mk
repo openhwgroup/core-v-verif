93a1a5c4705ff878be43c77876982bb2c5e4cd75
Yoann Pruvost <yoann.pruvost@dolphin.fr>
2023-03-31 10:11:00 +0800

    Switching core to fork

--- cv32e40p/sim/ExternalRepos.mk
+++ cv32e40p/sim/ExternalRepos.mk
@@ -13,10 +13,15 @@
 
 export SHELL = /bin/bash
 
-CV_CORE_REPO   ?= https://github.com/openhwgroup/cv32e40p
-CV_CORE_BRANCH ?= dev
-CV_CORE_HASH   ?= c2a0697dcc1521570d685a491859849bf9b54fbd
+# CV_CORE_REPO   ?= https://github.com/openhwgroup/cv32e40p
+# CV_CORE_BRANCH ?= dev
+# CV_CORE_HASH   ?= c2a0697dcc1521570d685a491859849bf9b54fbd
 CV_CORE_TAG    ?= none
+CV_CORE_REPO   ?= git@github.com:YoannPruvost/cv32e40p.git
+CV_CORE_BRANCH ?= dev
+CV_CORE_HASH   ?= head
+
+
 # The CV_CORE_HASH above points to version of the RTL that is newer.
 # It is logically equivalent with respect to v1.0.0 RTL freeze version with all parameters disabled.
 # There are some implementation and testbench updates in the above hash.
