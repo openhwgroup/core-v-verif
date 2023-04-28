097087ca6d5a76560099393f0cfb4f0e278fc4db
Kristine Dosvik <krdosvik@silabs.com>
2023-04-28 17:20:52 +0200

    use helper function and remove comment
    Signed-off-by: Kristine Dosvik <krdosvik@silabs.com>

--- cv32e40s/tb/uvmt/uvmt_cv32e40s_xsecure_assert/uvmt_cv32e40s_xsecure_data_independent_timing_assert.sv
+++ cv32e40s/tb/uvmt/uvmt_cv32e40s_xsecure_assert/uvmt_cv32e40s_xsecure_data_independent_timing_assert.sv
@@ -76,8 +76,7 @@ module uvmt_cv32e40s_xsecure_data_independent_timing_assert
     //There is therefor only 1 empty cycle after a branch instruction.
 
   sequence seq_no_mem_instr_for_cycles(x);
-    //(!rvfi_if.is_mem_act)[*x];
-    (!(((|rvfi_if.rvfi_mem_rmask) || (|rvfi_if.rvfi_mem_wmask)) && rvfi_if.rvfi_valid))[*x];
+    (!rvfi_if.is_mem_act)[*x];
   endsequence
 
   a_xsecure_dataindtiming_branch_timing_pc_hardening_disabled: assert property (
