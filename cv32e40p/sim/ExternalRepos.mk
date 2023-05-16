630c9f316618dd8b398257e81bb6f94d7568c1fd
Pascal Gouedo <pascal.gouedo@dolphin.fr>
2023-05-16 15:14:58 +0200

    v1.3.1 aligned RTL hash.
    Signed-off-by: Pascal Gouedo <pascal.gouedo@dolphin.fr>

--- cv32e40p/sim/ExternalRepos.mk
+++ cv32e40p/sim/ExternalRepos.mk
@@ -15,7 +15,7 @@ export SHELL = /bin/bash
 
 CV_CORE_REPO   ?= https://github.com/openhwgroup/cv32e40p
 CV_CORE_BRANCH ?= dev
-CV_CORE_HASH   ?= 4d960913d69e2043df80ac249ea17a008b67aaa6
+CV_CORE_HASH   ?= f789eb83bce527664867cae9d21a584b28492935
 CV_CORE_TAG    ?= none
 
 # The CV_CORE_HASH above points to version of the RTL that is newer.
