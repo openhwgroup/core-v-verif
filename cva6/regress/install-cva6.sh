34d952747abd38218c743e80461e656d25d41dc1
Abdul Wadood <abdulwadood.afzal88@gmail.com>
2023-05-22 08:38:12 +0500

    Updated CVA6 HASH
    Signed-off-by: Abdul Wadood <abdulwadood.afzal88@gmail.com>

--- cva6/regress/install-cva6.sh
+++ cva6/regress/install-cva6.sh
@@ -67,7 +67,7 @@ export NUM_JOBS=24
 if [ -z "$CVA6_REPO" ]; then
   CVA6_REPO="https://github.com/openhwgroup/cva6.git"
   CVA6_BRANCH="master"
-  CVA6_HASH="4f06aa620f75bcae369f05d0652283d45ef76a24"
+  CVA6_HASH="24841d307b77a6031f64377cb184dc8157902739"
   CVA6_PATCH=
 fi
 echo $CVA6_REPO
