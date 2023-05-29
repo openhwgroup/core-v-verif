cc2af8bcec1773115c5e9fba286bb66bc13b2aa4
bsm <baoshan.mak@dolphin.fr>
2023-05-29 20:37:49 +0800

    Update header with copyright
    Signed-off-by: bsm <baoshan.mak@dolphin.fr>

--- cv32e40p/env/corev-dv/instr_lib/cv32e40p_float_instr_lib.sv
+++ cv32e40p/env/corev-dv/instr_lib/cv32e40p_float_instr_lib.sv
@@ -2,6 +2,7 @@
  * Copyright 2018 Google LLC
  * Copyright 2020 Andes Technology Co., Ltd.
  * Copyright 2023 Dolphin Design
+ * SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
