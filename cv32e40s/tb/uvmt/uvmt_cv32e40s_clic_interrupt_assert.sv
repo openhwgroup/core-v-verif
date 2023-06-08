93e066499b3f412b771c636450278741ace9d136
Henrik Fegran <Henrik.Fegran@silabs.com>
2023-06-08 08:53:19 +0200

    Fixed only one ack per irq clic assertion
    Signed-off-by: Henrik Fegran <Henrik.Fegran@silabs.com>

--- cv32e40s/tb/uvmt/uvmt_cv32e40s_clic_interrupt_assert.sv
+++ cv32e40s/tb/uvmt/uvmt_cv32e40s_clic_interrupt_assert.sv
@@ -1068,10 +1068,19 @@ module uvmt_cv32e40s_clic_interrupt_assert
     // Only one irq_ack per irq
     // ------------------------------------------------------------------------
 
+    //property p_only_one_ack_per_irq;
+    //    clic.irq
+    //  |->
+    //    irq_ack[=0:1] within $changed(clic)[->1];
+    //endproperty : p_only_one_ack_per_irq
+
     property p_only_one_ack_per_irq;
         clic.irq
-      |->
-        irq_ack[=0:1] within $changed(clic)[->1];
+        && $changed(clic)
+      |=>
+        irq_ack[=1] within ($changed(clic)[->1])
+      or
+        !irq_ack until ($changed(clic)[->1]);
     endproperty : p_only_one_ack_per_irq
 
     a_only_one_ack_per_irq: assert property (p_only_one_ack_per_irq)
