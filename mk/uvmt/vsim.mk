cc0f2fb7f74b49be42e37ebdc1d3aeb0aa88c939
Xavier Aubert <xavier.aubert@dolphin.fr>
2023-07-31 09:32:17 +0200

    added back one missing feature from discarded PR#1972, corev-dv is not compiled if COMP=0 option is set

--- mk/uvmt/vsim.mk
+++ mk/uvmt/vsim.mk
@@ -191,6 +191,7 @@ VSIM_FLAGS += -sv_lib $(basename $(abspath $(SVLIB_LIB)))
 # Skip compile if requested (COMP=NO)
 ifneq ($(call IS_NO,$(COMP)),NO)
 VSIM_SIM_PREREQ = comp
+VSIM_COREVDV_SIM_PREREQ = comp_corev-dv
 endif
 
 ################################################################################
@@ -378,7 +379,7 @@ vopt_corev-dv:
 			-o $(CV_CORE_LC)_instr_gen_tb_top_vopt \
 			-l vopt.log
 
-gen_corev-dv: comp_corev-dv
+gen_corev-dv: $(VSIM_COREVDV_SIM_PREREQ)
 	mkdir -p $(SIM_COREVDV_RESULTS)/$(TEST)
 	for (( idx=${GEN_START_INDEX}; idx < $$((${GEN_START_INDEX} + ${GEN_NUM_TESTS})); idx++ )); do \
 		mkdir -p $(SIM_TEST_RESULTS)/$$idx/test_program; \
