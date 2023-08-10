7b6a20ad643c8e9a5f376a022d33730607040b0d
Vaibhav Jain <Vaibhav.Jain@dolphin.fr>
2023-08-10 17:59:35 +0800

    update cv32e40p core hash
    Signed-off-by: Vaibhav Jain <Vaibhav.Jain@dolphin.fr>

--- cv32e40p/sim/ExternalRepos.mk
+++ cv32e40p/sim/ExternalRepos.mk
@@ -15,7 +15,7 @@ export SHELL = /bin/bash
 
 CV_CORE_REPO   ?= https://github.com/openhwgroup/cv32e40p
 CV_CORE_BRANCH ?= dev
-CV_CORE_HASH   ?= 25907a07dc1e4557141e2bca957f35ce28cf6dad
+CV_CORE_HASH   ?= 73ff0c15eef61cf7f1882dbf6f62ce5ada1216f3
 CV_CORE_TAG    ?= none
 
 # The CV_CORE_HASH above points to version of the RTL that is newer.
