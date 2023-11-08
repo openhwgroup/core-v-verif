4c66e83b8282980eaeaf747a4d9d4cb222e6e48f
bsm <baoshan.mak@dolphin.fr>
2023-11-08 10:54:02 +0800

    Minor cleanup
    Signed-off-by: bsm <baoshan.mak@dolphin.fr>

--- cv32e40p/env/uvme/cov/uvme_rv32x_hwloop_covg.sv
+++ cv32e40p/env/uvme/cov/uvme_rv32x_hwloop_covg.sv
@@ -68,7 +68,7 @@ class uvme_rv32x_hwloop_covg # (
   local s_hwloop_stat       hwloop_stat_``TYPE  = '{default:0, hwloop_type:NULL_TYPE, hwloop_setup:'{default:NULL_SETUP}}; \
   local bit [(ILEN-1):0]    insn_list_in_hwloop_``TYPE      [HWLOOP_NB][$]; \
   local bit [31:0]          irq_vect_``TYPE                 [HWLOOP_NB][$]; \
-  local bit                 debug_req_``TYPE                 [HWLOOP_NB] = '{default:0}; \
+  local bit                 debug_req_``TYPE                [HWLOOP_NB] = '{default:0}; \
   local bit                 dbg_trigger_``TYPE              [HWLOOP_NB] = '{default:0}; \
   local int unsigned        dbg_step_cnt_``TYPE             [HWLOOP_NB] = '{default:0}; \
   local bit                 done_insn_list_capture_``TYPE   [HWLOOP_NB] = '{default:0};
@@ -661,15 +661,15 @@ class uvme_rv32x_hwloop_covg # (
       end \
       if (csr_hwloop_``TYPE``.lp_start_wb[i] && csr_hwloop_``TYPE``.lp_end_wb[i] && csr_hwloop_``TYPE``.lp_count_wb[i]) begin : SAMPLE_HWLOP_CSR \
         if (csr_hwloop_``TYPE``.lp_count[i] != 0) begin : SAMPLE_IF_TRUE \
-          `uvm_info(_header, $sformatf("DEBUG - cg_csr_hwloop[%0d] - sampling csr_hwloop is %p", i, csr_hwloop_``TYPE``), UVM_NONE); \
+          `uvm_info(_header, $sformatf("DEBUG - cg_csr_hwloop[%0d] - sampling csr_hwloop is %p", i, csr_hwloop_``TYPE``), UVM_DEBUG); \
           unique case (i) \
             0:  begin \
                 `CG_CSR_HWLOOP(0).sample(csr_hwloop_``TYPE``); \
-                `uvm_info(_header, $sformatf("DEBUG - cg_csr_hwloop[%0d] - get_inst_coverage = %.2f, get_coverage = %.2f", i, `CG_CSR_HWLOOP(0).get_inst_coverage(), `CG_CSR_HWLOOP(0).get_coverage), UVM_NONE); \
+                `uvm_info(_header, $sformatf("DEBUG - cg_csr_hwloop[%0d] - get_inst_coverage = %.2f, get_coverage = %.2f", i, `CG_CSR_HWLOOP(0).get_inst_coverage(), `CG_CSR_HWLOOP(0).get_coverage), UVM_DEBUG); \
                 end \
             1:  begin \
                 `CG_CSR_HWLOOP(1).sample(csr_hwloop_``TYPE``); \
-                `uvm_info(_header, $sformatf("DEBUG - cg_csr_hwloop[%0d] - get_inst_coverage = %.2f, get_coverage = %.2f", i, `CG_CSR_HWLOOP(1).get_inst_coverage(), `CG_CSR_HWLOOP(1).get_coverage), UVM_NONE); \
+                `uvm_info(_header, $sformatf("DEBUG - cg_csr_hwloop[%0d] - get_inst_coverage = %.2f, get_coverage = %.2f", i, `CG_CSR_HWLOOP(1).get_inst_coverage(), `CG_CSR_HWLOOP(1).get_coverage), UVM_DEBUG); \
                 end \
           endcase \
           // update hwloop_stat \
@@ -690,8 +690,6 @@ class uvme_rv32x_hwloop_covg # (
   `DEF_CHECK_N_SAMPLE_CSR_HWLOOP(sub)
 
   // task to sample cg_features_of_hwloop
-  string sub = "sub";
-  string main = "main";
   `define CHECK_N_SAMPLE_HWLOOP(TYPE) check_n_sample_hwloop_``TYPE``();
   `define DEF_CHECK_N_SAMPLE_HWLOOP(TYPE) task check_n_sample_hwloop_``TYPE``(); \
     for (int i=0; i<HWLOOP_NB; i++) begin : UPDATE_HWLOOP_STAT \
@@ -760,7 +758,7 @@ class uvme_rv32x_hwloop_covg # (
           insn_item = insn_list_in_hwloop_``TYPE``[i].pop_front(); \
           if (irq_vect_``TYPE``[i].size() != 0) begin \
             irq_item = irq_vect_``TYPE``[i].pop_front(); \
-            `uvm_info(_header, $sformatf("DEBUG - COLLECT_IRQ - idx:%0d - irq_item is %8h", i, irq_item), UVM_NONE); \
+            `uvm_info(_header, $sformatf("DEBUG - COLLECT_IRQ - idx:%0d - irq_item is %8h", i, irq_item), UVM_DEBUG); \
           end \
           if (debug_req_``TYPE``[i] != 0) begin \
             debug_req_item = debug_req_``TYPE``[i]; \
@@ -774,7 +772,7 @@ class uvme_rv32x_hwloop_covg # (
             dbg_step_cnt_item = dbg_step_cnt_``TYPE``[i]; \
             dbg_step_cnt_``TYPE``[i] = 0; \
           end \
-          `uvm_info(_header, $sformatf("DEBUG - COLLECT_INSTR - idx:%0d - insn_item is %8h", i, insn_item), UVM_NONE); \
+          `uvm_info(_header, $sformatf("DEBUG - COLLECT_INSTR - idx:%0d - insn_item is %8h", i, insn_item), UVM_DEBUG); \
           unique case (i) \
             0:  begin \
                   `CG_FEATURES_OF_HWLOOP(0).sample(hwloop_stat_``TYPE``, insn_item, irq_item, debug_req_item, dbg_trigger_item, dbg_step_cnt_item); \
@@ -805,10 +803,10 @@ class uvme_rv32x_hwloop_covg # (
           INSTR_ECALL  :               is_ecall   = 1;
           default      :               is_illegal = 1;
         endcase
-        `uvm_info(_header, $sformatf("DEBUG - EXCEPTION Entry due to %s", exception_code.name()), UVM_NONE);
+        `uvm_info(_header, $sformatf("DEBUG - EXCEPTION Entry due to %s", exception_code.name()), UVM_DEBUG);
         wait (cv32e40p_rvvi_vif.clk && cv32e40p_rvvi_vif.valid && cv32e40p_rvvi_vif.insn == INSTR_MRET);
         is_ebreak = 0; is_ecall = 0; is_illegal = 0;
-        `uvm_info(_header, $sformatf("DEBUG - EXCEPTION Exit"), UVM_NONE);
+        `uvm_info(_header, $sformatf("DEBUG - EXCEPTION Exit"), UVM_DEBUG);
       end // EXCEPTION_HANDLING
       forever begin : IRQ_ENTRY
         wait (hwloop_stat_main.execute_instr_in_hwloop[0] | hwloop_stat_main.execute_instr_in_hwloop[1] | hwloop_stat_sub.execute_instr_in_hwloop[0]  | hwloop_stat_sub.execute_instr_in_hwloop[1]);
@@ -817,14 +815,14 @@ class uvme_rv32x_hwloop_covg # (
         if (hwloop_stat_main.execute_instr_in_hwloop[1] && hwloop_stat_main.track_lp_count[1] !=0 && !in_nested_loop0)  irq_vect_main[1].push_back(cv32e40p_rvvi_vif.irq_onehot_priority);
         if (hwloop_stat_sub.execute_instr_in_hwloop[0]  && hwloop_stat_sub.track_lp_count[0] !=0)                       irq_vect_sub[0].push_back(cv32e40p_rvvi_vif.irq_onehot_priority);
         if (hwloop_stat_sub.execute_instr_in_hwloop[1]  && hwloop_stat_sub.track_lp_count[1] !=0 && !in_nested_loop0)   irq_vect_sub[1].push_back(cv32e40p_rvvi_vif.irq_onehot_priority);
-        `uvm_info(_header, $sformatf("DEBUG - IRQ Entry"), UVM_NONE);
+        `uvm_info(_header, $sformatf("DEBUG - IRQ Entry"), UVM_DEBUG);
         is_irq = 1;
       end // IRQ_ENTRY
       forever begin : IRQ_EXIT
         wait (hwloop_stat_main.execute_instr_in_hwloop[0] | hwloop_stat_main.execute_instr_in_hwloop[1] | hwloop_stat_sub.execute_instr_in_hwloop[0]  | hwloop_stat_sub.execute_instr_in_hwloop[1]);
         @(posedge cv32e40p_rvvi_vif.clk);
         if (is_irq && cv32e40p_rvvi_vif.valid && cv32e40p_rvvi_vif.insn == INSTR_MRET) begin
-          `uvm_info(_header, $sformatf("DEBUG - IRQ Exit"), UVM_NONE);
+          `uvm_info(_header, $sformatf("DEBUG - IRQ Exit"), UVM_DEBUG);
           is_irq = 0;
         end
       end // IRQ_EXIT
@@ -854,7 +852,7 @@ class uvme_rv32x_hwloop_covg # (
                         if (hwloop_stat_sub.execute_instr_in_hwloop[1]  && hwloop_stat_sub.track_lp_count[1]  !=0 && !in_nested_loop0)  dbg_step_cnt_sub[1]++;
                       end
           endcase
-          `uvm_info(_header, $sformatf("DEBUG - Debug Mode Entry due to %s", dcsr_cause.name()), UVM_NONE);
+          `uvm_info(_header, $sformatf("DEBUG - Debug Mode Entry due to %s", dcsr_cause.name()), UVM_DEBUG);
           is_dbg_mode = 1;
         end
       end // DBG_ENTRY
@@ -863,7 +861,7 @@ class uvme_rv32x_hwloop_covg # (
         wait (is_dbg_mode);
         wait (cv32e40p_rvvi_vif.clk && cv32e40p_rvvi_vif.valid && cv32e40p_rvvi_vif.insn == INSTR_DRET) begin
           @(posedge cv32e40p_rvvi_vif.clk) ; @(negedge cv32e40p_rvvi_vif.clk);
-          `uvm_info(_header, $sformatf("DEBUG - Debug Mode Exit"), UVM_NONE);
+          `uvm_info(_header, $sformatf("DEBUG - Debug Mode Exit"), UVM_DEBUG);
           is_dbg_mode = 0;
         end
       end // DBG_EXIT
@@ -893,7 +891,7 @@ class uvme_rv32x_hwloop_covg # (
   function void final_phase(uvm_phase phase);
     super.final_phase(phase);
     if (hwloop_stat_main == hwloop_stat_init && hwloop_stat_sub == hwloop_stat_init) begin
-      `uvm_info(_header, $sformatf("DEBUG - No prematured hwloops when test done"), UVM_NONE);
+      `uvm_info(_header, $sformatf("DEBUG - No prematured hwloops when test done"), UVM_DEBUG);
     end
     else begin
       `uvm_error(_header, $sformatf("Detected prematured hwloops when test done. Please debug ... "));
