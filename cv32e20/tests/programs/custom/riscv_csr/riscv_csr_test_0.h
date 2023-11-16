17f405b05b92dccfac00959d785af1dc5e4bce34
Erich Gens <egens@intrinsix.com>
2023-11-16 15:07:42 -0500

    Added USER mode testing as well as privilege transition functions.

---
+++ cv32e20/tests/programs/custom/riscv_csr/riscv_csr_test_0.h
@@ -0,0 +1,7 @@
+/* This is an auto-generated file not intended to be hand edited.
+ * Changes should be made by editing the yaml file ../../../../env/corev-dv/cv32e20_csr_template.yaml
+ * Then invoking gen_csr_test.py to regenerate this file.
+ */
+void interrupt_csr_check();
+void machine_mode_check();
+void user_mode_check();
