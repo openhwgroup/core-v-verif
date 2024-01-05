884e36244db144f337f73599555ffaa4c2113332
AEzzejjari <110914586+AEzzejjari@users.noreply.github.com>
2024-01-05 16:05:46 +0100

    Update uvma_axi_fw_preload_seq.sv (Modify the preload memory method)

--- lib/uvm_agents/uvma_axi5/src/seq/uvma_axi_fw_preload_seq.sv
+++ lib/uvm_agents/uvma_axi5/src/seq/uvma_axi_fw_preload_seq.sv
@@ -13,23 +13,15 @@
 
 
 /**
- * Virtual sequence preload the CVA6 memory.
+ * Virtual sequence preload memory.
  */
 class uvma_axi_fw_preload_seq_c extends uvm_sequence;
 
-   static uvm_cmdline_processor uvcl = uvm_cmdline_processor::get_inst();
-            // Agent handles
+   // Agent handles
    uvma_axi_cfg_c    cfg;
    uvma_axi_cntxt_c  cntxt;
 
-   uvml_mem_c mem;
-
-   bit[63:0] value;
-   logic [7:0][7:0] mem_row;
-   string binary = "";
-   longint address;
-   longint len;
-   byte buffer[];
+   string fw_file_path;
 
    `uvm_object_utils(uvma_axi_fw_preload_seq_c)
    `uvm_declare_p_sequencer(uvma_axi_vsqr_c)
@@ -46,43 +38,13 @@ endclass : uvma_axi_fw_preload_seq_c
 function uvma_axi_fw_preload_seq_c::new(string name="uvma_axi_fw_preload_seq");
 
    super.new(name);
-   mem = uvml_mem_c::type_id::create("mem");
-   mem.mem_default = MEM_DEFAULT_0;
 
 endfunction : new
 
 task uvma_axi_fw_preload_seq_c::body();
 
-   cfg   = p_sequencer.cfg  ;
-   void'(uvcl.get_arg_value("+elf_file=", binary));
-
-   if (binary != "") begin
-      void'(read_elf(binary));
-      wait(p_sequencer.cntxt.axi_vi.clk);
-      // while there are more sections to process
-      while (get_section(address, len)) begin
-         automatic int num_words0 = (len+7)/8;
-         `uvm_info( "Core Test", $sformatf("Loading Address: %x, Length: %x", address, len), UVM_HIGH)
-         buffer = new [num_words0*8];
-         void'(read_section_sv(address, buffer));
-         // preload memories
-         // 64-bit
-         for (int i = 0; i < num_words0; i++) begin
-            mem_row = '0;
-            for (int j = 0; j < 8; j++) begin
-                mem_row[j] = buffer[i*8 + j];
-            end
-            mem.write(address + i*8 + 0, mem_row[0]);
-            mem.write(address + i*8 + 1, mem_row[1]);
-            mem.write(address + i*8 + 2, mem_row[2]);
-            mem.write(address + i*8 + 3, mem_row[3]);
-            mem.write(address + i*8 + 4, mem_row[4]);
-            mem.write(address + i*8 + 5, mem_row[5]);
-            mem.write(address + i*8 + 6, mem_row[6]);
-            mem.write(address + i*8 + 7, mem_row[7]);
-         end
-          p_sequencer.cntxt.mem = mem;
-      end
+   if ($value$plusargs("firmware=%s", fw_file_path)) begin
+      cntxt.mem.readmemh(fw_file_path);
    end
 
 endtask : body
