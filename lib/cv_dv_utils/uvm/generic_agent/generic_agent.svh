de367051ba9be6034abfe58ddf3b8fa8606ee687
Ludovic Pion <ludovic.pion@cea.fr>
2024-06-20 13:10:54 +0200

    [dv_utils generic_agent] by default it is an active agent now | [dv_utils tp_trans] added a python script to create OHG verification TestPlan | [dv_utils bp] added force and release task

--- lib/cv_dv_utils/uvm/generic_agent/generic_agent.svh
+++ lib/cv_dv_utils/uvm/generic_agent/generic_agent.svh
@@ -1,22 +1,19 @@
 // ----------------------------------------------------------------------------
-// Copyright 2023 CEA*
-// *Commissariat a l'Energie Atomique et aux Energies Alternatives (CEA)
+//Copyright 2023 CEA*
+//*Commissariat a l'Energie Atomique et aux Energies Alternatives (CEA)
 //
-// SPDX-License-Identifier: Apache-2.0
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
 //
 //    http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
 //[END OF HEADER]
-// ----------------------------------------------------------------------------
 // ----------------------------------------------------------------------------
 // ----------------------------------------------------------------------------
 //  Description : Agent for the generic request. 
@@ -35,7 +32,7 @@ class generic_agent #(type req_t, type rsp_t) extends uvm_agent;
   // -------------------------------------------------------------------------
   // Fields for the vrp agents
   // -------------------------------------------------------------------------
-  protected uvm_active_passive_enum is_active = UVM_PASSIVE;
+  protected uvm_active_passive_enum is_active = UVM_ACTIVE;
 
   generic_sequencer  #(req_t, rsp_t)m_sequencer;
   generic_driver     #(req_t, rsp_t)m_driver;
