d34611f7860aac9a73abac5c70c909de0114d7ba
Xiaowei Zhang <xiaowei.zhang@corelabtech.com>
2024-06-25 16:28:49 +0800

    if macro not defined, then define it

--- lib/uvm_agents/uvma_axi5/src/uvma_axi_macros.sv
+++ lib/uvm_agents/uvma_axi5/src/uvma_axi_macros.sv
@@ -9,15 +9,20 @@
 `ifndef __UVMA_AXI_MACROS_SV__
   `define __UVMA_AXI_MACROS_SV__
 
-  `define UVMA_AXI_ADDR_MAX_WIDTH           64
-  `define UVMA_AXI_DATA_MAX_WIDTH           64
-  `define UVMA_AXI_USER_MAX_WIDTH           512
-  `define UVMA_AXI_ID_MAX_WIDTH             64
-  // `define UVMA_AXI_STRB_MAX_WIDTH           8
+  `define IFNDEF_DEFINE(name,value) \
+    `ifndef name \
+      `define name value \
+  `endif
 
-  `define UVMA_AXI_MAX_NB_TXN_BURST         256
-  `define UVMA_AXI_LOOP_MAX_WIDTH           8
-  `define UVMA_AXI_MMUSID_MAX_WIDTH         32
-  `define UVMA_AXI_MMUSSID_MAX_WIDTH        20
+  `IFNDEF_DEFINE(UVMA_AXI_ADDR_MAX_WIDTH    , 64  )
+  `IFNDEF_DEFINE(UVMA_AXI_DATA_MAX_WIDTH    , 64  )
+  `IFNDEF_DEFINE(UVMA_AXI_USER_MAX_WIDTH    , 512 )
+  `IFNDEF_DEFINE(UVMA_AXI_ID_MAX_WIDTH      , 64  )
+  // `IFNDEF_DEFINE(UVMA_AXI_STRB_MAX_WIDTH , 8   )
+
+  `IFNDEF_DEFINE(UVMA_AXI_MAX_NB_TXN_BURST  , 256 )
+  `IFNDEF_DEFINE(UVMA_AXI_LOOP_MAX_WIDTH    , 8   )
+  `IFNDEF_DEFINE(UVMA_AXI_MMUSID_MAX_WIDTH  , 32  )
+  `IFNDEF_DEFINE(UVMA_AXI_MMUSSID_MAX_WIDTH , 20  )
 
 `endif // __UVMA_AXI_MACROS_SV__
