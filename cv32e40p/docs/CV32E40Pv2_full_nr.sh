ce554a076fa91af3c6fd0dfaa05a13a707561c99
Xavier Aubert <xavier.aubert@dolphin.fr>
2024-06-27 10:37:44 +0200

    improved file layout & added comments

--- cv32e40p/docs/CV32E40Pv2_full_nr.sh
+++ cv32e40p/docs/CV32E40Pv2_full_nr.sh
@@ -19,8 +19,10 @@
 # SPDX-License-Identifier: Apache-2.0 WITH SHL-2.0
 #
 ###############################################################################
+#
 # CV32E40Pv2_full_nr: lists the needed commands to reproduce the NR as performed
-# by dolphin design team suring CV32E40Pv2 verification
+# by dolphin design team suring CV32E40Pv2 verification, using shell script only
+# For the closest reproduction, consider using CV32E40Pv2_full_nr_questa_vrun.sh
 #
 # Usage:
 #       To reproduce the full NR, simply execute:
@@ -32,7 +34,10 @@
 #       bin folder to generate your own sub-regress file if only one
 #       config/regress file combo is desired
 #
-# CV_VERIF_PATH (core-v-verif root path) should be defined before running this script
+# - CV_VERIF_PATH (core-v-verif root path) should be defined before running this script
+# - All needed tools should be sourced before running the script
+# (simulator, imperas ISS, etc.)
+#
 ###############################################################################
 
 if [[ -z "${CV_VERIF_PATH-}" ]]; then
@@ -42,73 +47,141 @@ fi
 
 pushd ${CV_VERIF_PATH}/bin > /dev/null
 
+# CFG pulp | NR File xpulp_instr
 ./cv_regress --sh --file=cv32e40pv2_xpulp_instr.yaml --simulator=vsim --outfile=pulp_xpulp_instr.sh --num=200 --iss yes --cov --cfg pulp --add_test_cfg disable_all_trn_logs
 sh pulp_xpulp_instr.sh
+
+# CFG pulp | NR File interrupt_debug_short
 ./cv_regress --sh --file=cv32e40pv2_interrupt_debug_short.yaml --simulator=vsim --outfile=pulp_interrupt_debug_short.sh --num=60 --iss yes --cov --cfg pulp --add_test_cfg disable_all_trn_logs
 sh pulp_interrupt_debug_short.sh
+
+# CFG pulp | NR File interrupt_debug_long
 ./cv_regress --sh --file=cv32e40pv2_interrupt_debug_long.yaml --simulator=vsim --outfile=pulp_interrupt_debug_long.sh --num=6 --iss yes --cov --cfg pulp --add_test_cfg disable_all_trn_logs
 sh pulp_interrupt_debug_long.sh
+
+# CFG pulp | NR File legacy_v1
 ./cv_regress --sh --file=cv32e40pv2_legacy_v1.yaml --simulator=vsim --outfile=pulp_legacy_v1.sh --num=1 --iss yes --cov --cfg pulp --add_test_cfg disable_all_trn_logs
 sh pulp_legacy_v1.sh
+
+# CFG pulp_fpu | NR File xpulp_instr
 ./cv_regress --sh --file=cv32e40pv2_xpulp_instr.yaml --simulator=vsim --outfile=pulp_fpu_xpulp_instr.sh --num=200 --iss yes --cov --cfg pulp_fpu --add_test_cfg disable_all_trn_logs,floating_pt_instr_en
 sh pulp_fpu_xpulp_instr.sh
+
+# CFG pulp_fpu | NR File fpu_instr
 ./cv_regress --sh --file=cv32e40pv2_fpu_instr.yaml --simulator=vsim --outfile=pulp_fpu_fpu_instr.sh --num=300 --iss yes --cov --cfg pulp_fpu --add_test_cfg disable_all_trn_logs,floating_pt_instr_en
 sh pulp_fpu_fpu_instr.sh
+
+# CFG pulp_fpu | NR File interrupt_debug_short
 ./cv_regress --sh --file=cv32e40pv2_interrupt_debug_short.yaml --simulator=vsim --outfile=pulp_fpu_interrupt_debug_short.sh --num=60 --iss yes --cov --cfg pulp_fpu --add_test_cfg disable_all_trn_logs,floating_pt_instr_en
 sh pulp_fpu_interrupt_debug_short.sh
+
+# CFG pulp_fpu | NR File interrupt_debug_long
 ./cv_regress --sh --file=cv32e40pv2_interrupt_debug_long.yaml --simulator=vsim --outfile=pulp_fpu_interrupt_debug_long.sh --num=6 --iss yes --cov --cfg pulp_fpu --add_test_cfg disable_all_trn_logs,floating_pt_instr_en
 sh pulp_fpu_interrupt_debug_long.sh
+
+# CFG pulp_fpu | NR File legacy_v1
 ./cv_regress --sh --file=cv32e40pv2_legacy_v1.yaml --simulator=vsim --outfile=pulp_fpu_legacy_v1.sh --num=1 --iss yes --cov --cfg pulp_fpu --add_test_cfg disable_all_trn_logs,floating_pt_instr_en
 sh pulp_fpu_legacy_v1.sh
+
+# CFG pulp_fpu_1cyclat | NR File xpulp_instr
 ./cv_regress --sh --file=cv32e40pv2_xpulp_instr.yaml --simulator=vsim --outfile=pulp_fpu_1cyclat_xpulp_instr.sh --num=200 --iss yes --cov --cfg pulp_fpu_1cyclat --add_test_cfg disable_all_trn_logs,floating_pt_instr_en
 sh pulp_fpu_1cyclat_xpulp_instr.sh
+
+# CFG pulp_fpu_1cyclat | NR File fpu_instr
 ./cv_regress --sh --file=cv32e40pv2_fpu_instr.yaml --simulator=vsim --outfile=pulp_fpu_1cyclat_fpu_instr.sh --num=300 --iss yes --cov --cfg pulp_fpu_1cyclat --add_test_cfg disable_all_trn_logs,floating_pt_instr_en
 sh pulp_fpu_1cyclat_fpu_instr.sh
+
+# CFG pulp_fpu_1cyclat | NR File interrupt_debug_short
 ./cv_regress --sh --file=cv32e40pv2_interrupt_debug_short.yaml --simulator=vsim --outfile=pulp_fpu_1cyclat_interrupt_debug_short.sh --num=60 --iss yes --cov --cfg pulp_fpu_1cyclat --add_test_cfg disable_all_trn_logs,floating_pt_instr_en
 sh pulp_fpu_1cyclat_interrupt_debug_short.sh
+
+# CFG pulp_fpu_1cyclat | NR File interrupt_debug_long
 ./cv_regress --sh --file=cv32e40pv2_interrupt_debug_long.yaml --simulator=vsim --outfile=pulp_fpu_1cyclat_interrupt_debug_long.sh --num=6 --iss yes --cov --cfg pulp_fpu_1cyclat --add_test_cfg disable_all_trn_logs,floating_pt_instr_en
 sh pulp_fpu_1cyclat_interrupt_debug_long.sh
+
+# CFG pulp_fpu_1cyclat | NR File legacy_v1
 ./cv_regress --sh --file=cv32e40pv2_legacy_v1.yaml --simulator=vsim --outfile=pulp_fpu_1cyclat_legacy_v1.sh --num=1 --iss yes --cov --cfg pulp_fpu_1cyclat --add_test_cfg disable_all_trn_logs,floating_pt_instr_en
 sh pulp_fpu_1cyclat_legacy_v1.sh
+
+# CFG pulp_fpu_2cyclat | NR File xpulp_instr
 ./cv_regress --sh --file=cv32e40pv2_xpulp_instr.yaml --simulator=vsim --outfile=pulp_fpu_2cyclat_xpulp_instr.sh --num=200 --iss yes --cov --cfg pulp_fpu_2cyclat --add_test_cfg disable_all_trn_logs,floating_pt_instr_en
 sh pulp_fpu_2cyclat_xpulp_instr.sh
+
+# CFG pulp_fpu_2cyclat | NR File fpu_instr
 ./cv_regress --sh --file=cv32e40pv2_fpu_instr.yaml --simulator=vsim --outfile=pulp_fpu_2cyclat_fpu_instr.sh --num=300 --iss yes --cov --cfg pulp_fpu_2cyclat --add_test_cfg disable_all_trn_logs,floating_pt_instr_en
 sh pulp_fpu_2cyclat_fpu_instr.sh
+
+# CFG pulp_fpu_2cyclat | NR File interrupt_debug_short
 ./cv_regress --sh --file=cv32e40pv2_interrupt_debug_short.yaml --simulator=vsim --outfile=pulp_fpu_2cyclat_interrupt_debug_short.sh --num=60 --iss yes --cov --cfg pulp_fpu_2cyclat --add_test_cfg disable_all_trn_logs,floating_pt_instr_en
 sh pulp_fpu_2cyclat_interrupt_debug_short.sh
+
+# CFG pulp_fpu_2cyclat | NR File interrupt_debug_long
 ./cv_regress --sh --file=cv32e40pv2_interrupt_debug_long.yaml --simulator=vsim --outfile=pulp_fpu_2cyclat_interrupt_debug_long.sh --num=6 --iss yes --cov --cfg pulp_fpu_2cyclat --add_test_cfg disable_all_trn_logs,floating_pt_instr_en
 sh pulp_fpu_2cyclat_interrupt_debug_long.sh
+
+# CFG pulp_fpu_2cyclat | NR File legacy_v1
 ./cv_regress --sh --file=cv32e40pv2_legacy_v1.yaml --simulator=vsim --outfile=pulp_fpu_2cyclat_legacy_v1.sh --num=1 --iss yes --cov --cfg pulp_fpu_2cyclat --add_test_cfg disable_all_trn_logs,floating_pt_instr_en
 sh pulp_fpu_2cyclat_legacy_v1.sh
+
+# CFG pulp_fpu_zfinx | NR File xpulp_instr
 ./cv_regress --sh --file=cv32e40pv2_xpulp_instr.yaml --simulator=vsim --outfile=pulp_fpu_zfinx_xpulp_instr.sh --num=200 --iss yes --cov --cfg pulp_fpu_zfinx --add_test_cfg disable_all_trn_logs,floating_pt_zfinx_instr_en
 sh pulp_fpu_zfinx_xpulp_instr.sh
+
+# CFG pulp_fpu_zfinx | NR File fpu_instr
 ./cv_regress --sh --file=cv32e40pv2_fpu_instr.yaml --simulator=vsim --outfile=pulp_fpu_zfinx_fpu_instr.sh --num=300 --iss yes --cov --cfg pulp_fpu_zfinx --add_test_cfg disable_all_trn_logs,floating_pt_zfinx_instr_en
 sh pulp_fpu_zfinx_fpu_instr.sh
+
+# CFG pulp_fpu_zfinx | NR File interrupt_debug_short
 ./cv_regress --sh --file=cv32e40pv2_interrupt_debug_short.yaml --simulator=vsim --outfile=pulp_fpu_zfinx_interrupt_debug_short.sh --num=60 --iss yes --cov --cfg pulp_fpu_zfinx --add_test_cfg disable_all_trn_logs,floating_pt_zfinx_instr_en
 sh pulp_fpu_zfinx_interrupt_debug_short.sh
+
+# CFG pulp_fpu_zfinx | NR File interrupt_debug_long
 ./cv_regress --sh --file=cv32e40pv2_interrupt_debug_long.yaml --simulator=vsim --outfile=pulp_fpu_zfinx_interrupt_debug_long.sh --num=6 --iss yes --cov --cfg pulp_fpu_zfinx --add_test_cfg disable_all_trn_logs,floating_pt_zfinx_instr_en
 sh pulp_fpu_zfinx_interrupt_debug_long.sh
+
+# CFG pulp_fpu_zfinx | NR File legacy_v1
 ./cv_regress --sh --file=cv32e40pv2_legacy_v1.yaml --simulator=vsim --outfile=pulp_fpu_zfinx_legacy_v1.sh --num=1 --iss yes --cov --cfg pulp_fpu_zfinx --add_test_cfg disable_all_trn_logs,floating_pt_zfinx_instr_en
 sh pulp_fpu_zfinx_legacy_v1.sh
+
+# CFG pulp_fpu_zfinx_1cyclat | NR File xpulp_instr
 ./cv_regress --sh --file=cv32e40pv2_xpulp_instr.yaml --simulator=vsim --outfile=pulp_fpu_zfinx_1cyclat_xpulp_instr.sh --num=200 --iss yes --cov --cfg pulp_fpu_zfinx_1cyclat --add_test_cfg disable_all_trn_logs,floating_pt_zfinx_instr_en
 sh pulp_fpu_zfinx_1cyclat_xpulp_instr.sh
+
+# CFG pulp_fpu_zfinx_1cyclat | NR File fpu_instr
 ./cv_regress --sh --file=cv32e40pv2_fpu_instr.yaml --simulator=vsim --outfile=pulp_fpu_zfinx_1cyclat_fpu_instr.sh --num=300 --iss yes --cov --cfg pulp_fpu_zfinx_1cyclat --add_test_cfg disable_all_trn_logs,floating_pt_zfinx_instr_en
 sh pulp_fpu_zfinx_1cyclat_fpu_instr.sh
+
+# CFG pulp_fpu_zfinx_1cyclat | NR File interrupt_debug_short
 ./cv_regress --sh --file=cv32e40pv2_interrupt_debug_short.yaml --simulator=vsim --outfile=pulp_fpu_zfinx_1cyclat_interrupt_debug_short.sh --num=60 --iss yes --cov --cfg pulp_fpu_zfinx_1cyclat --add_test_cfg disable_all_trn_logs,floating_pt_zfinx_instr_en
 sh pulp_fpu_zfinx_1cyclat_interrupt_debug_short.sh
+
+# CFG pulp_fpu_zfinx_1cyclat | NR File interrupt_debug_long
 ./cv_regress --sh --file=cv32e40pv2_interrupt_debug_long.yaml --simulator=vsim --outfile=pulp_fpu_zfinx_1cyclat_interrupt_debug_long.sh --num=6 --iss yes --cov --cfg pulp_fpu_zfinx_1cyclat --add_test_cfg disable_all_trn_logs,floating_pt_zfinx_instr_en
 sh pulp_fpu_zfinx_1cyclat_interrupt_debug_long.sh
+
+# CFG pulp_fpu_zfinx_1cyclat | NR File legacy_v1
 ./cv_regress --sh --file=cv32e40pv2_legacy_v1.yaml --simulator=vsim --outfile=pulp_fpu_zfinx_1cyclat_legacy_v1.sh --num=1 --iss yes --cov --cfg pulp_fpu_zfinx_1cyclat --add_test_cfg disable_all_trn_logs,floating_pt_zfinx_instr_en
 sh pulp_fpu_zfinx_1cyclat_legacy_v1.sh
+
+# CFG pulp_fpu_zfinx_2cyclat | NR File xpulp_instr
 ./cv_regress --sh --file=cv32e40pv2_xpulp_instr.yaml --simulator=vsim --outfile=pulp_fpu_zfinx_2cyclat_xpulp_instr.sh --num=200 --iss yes --cov --cfg pulp_fpu_zfinx_2cyclat --add_test_cfg disable_all_trn_logs,floating_pt_zfinx_instr_en
 sh pulp_fpu_zfinx_2cyclat_xpulp_instr.sh
+
+# CFG pulp_fpu_zfinx_2cyclat | NR File fpu_instr
 ./cv_regress --sh --file=cv32e40pv2_fpu_instr.yaml --simulator=vsim --outfile=pulp_fpu_zfinx_2cyclat_fpu_instr.sh --num=300 --iss yes --cov --cfg pulp_fpu_zfinx_2cyclat --add_test_cfg disable_all_trn_logs,floating_pt_zfinx_instr_en
 sh pulp_fpu_zfinx_2cyclat_fpu_instr.sh
+
+# CFG pulp_fpu_zfinx_2cyclat | NR File interrupt_debug_short
 ./cv_regress --sh --file=cv32e40pv2_interrupt_debug_short.yaml --simulator=vsim --outfile=pulp_fpu_zfinx_2cyclat_interrupt_debug_short.sh --num=60 --iss yes --cov --cfg pulp_fpu_zfinx_2cyclat --add_test_cfg disable_all_trn_logs,floating_pt_zfinx_instr_en
 sh pulp_fpu_zfinx_2cyclat_interrupt_debug_short.sh
+
+# CFG pulp_fpu_zfinx_2cyclat | NR File interrupt_debug_long
 ./cv_regress --sh --file=cv32e40pv2_interrupt_debug_long.yaml --simulator=vsim --outfile=pulp_fpu_zfinx_2cyclat_interrupt_debug_long.sh --num=6 --iss yes --cov --cfg pulp_fpu_zfinx_2cyclat --add_test_cfg disable_all_trn_logs,floating_pt_zfinx_instr_en
 sh pulp_fpu_zfinx_2cyclat_interrupt_debug_long.sh
+
+# CFG pulp_fpu_zfinx_2cyclat | NR File legacy_v1
 ./cv_regress --sh --file=cv32e40pv2_legacy_v1.yaml --simulator=vsim --outfile=pulp_fpu_zfinx_2cyclat_legacy_v1.sh --num=1 --iss yes --cov --cfg pulp_fpu_zfinx_2cyclat --add_test_cfg disable_all_trn_logs,floating_pt_zfinx_instr_en
 sh pulp_fpu_zfinx_2cyclat_legacy_v1.sh
+
 
 popd > /dev/null
