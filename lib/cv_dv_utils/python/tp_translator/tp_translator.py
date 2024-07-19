8ae76c5c015571ba3a45d9fe626d4cab328111db
Ludovic Pion <ludovic.pion@cea.fr>
2024-07-19 16:10:51 +0200

    Added file list to be able to get rid of internal flow for compilation, [mem_rsp_model]: fixes related to atomic transaction, Updated translator script

--- lib/cv_dv_utils/python/tp_translator/tp_translator.py
+++ lib/cv_dv_utils/python/tp_translator/tp_translator.py
@@ -1,5 +1,5 @@
 # ----------------------------------------------------------------------------
-# Copyright 2023 CEA*
+# Copyright 2024 CEA*
 # *Commissariat a l'Energie Atomique et aux Energies Alternatives (CEA)
 #
 # SPDX-License-Identifier: Apache-2.0
@@ -23,11 +23,11 @@ args = parser.parse_args()
 
 
 ## Read Mapping Sheet
-df_mapping    = pd.read_excel(args.input_xls, sheet_name=3, dtype={'Features': str, 'Test Items': str})
-df_features   = pd.read_excel(args.input_xls, sheet_name=1)
-df_test_items = pd.read_excel(args.input_xls, sheet_name=2, skiprows=2, dtype={'Section': str, 'Title': str, 'Description': str})
+df_mapping    = pd.read_excel(args.input_xls, sheet_name=2, dtype={'Features': str, 'Test Items': str})
+df_features   = pd.read_excel(args.input_xls, sheet_name=0)
+df_test_items = pd.read_excel(args.input_xls, sheet_name=1, skiprows=2, dtype={'Section': str, 'Title': str, 'Description': str})
 
-#print(df_test_items)
+#print(df_mapping)
 top_dict = [];
 #for i, row in df_mapping.iterrows():
 #    top_dict.append({'Feature ID'          : row.iloc[0],
@@ -40,15 +40,19 @@ top_dict = [];
 #                     })
 #
 for i, row in df_mapping.iterrows():
-    top_dict.append({'Feature ID'          : df_features[df_features['Sub Section']==row.iloc[0]].iloc[0]['Section'],
+#    print(row.iloc[0], row.iloc[1])
+    top_dict.append({'Requirement ID'      : df_features[df_features['Sub Section']==row.iloc[0]].iloc[0]['Section'],
                      'Feature'             : df_features[df_features['Sub Section']==row.iloc[0]].iloc[0]['Feature'], 
-                     'Sub Feature ID'      : row.iloc[0],
                      'Sub Feature'         : df_features[df_features['Sub Section']==row.iloc[0]].iloc[0]['Sub Feature'], 
-                     'Test Items Title'    : df_test_items[df_test_items['Section']==row.iloc[1]].iloc[0]['Title'],
+                     'Feature Description' : df_features[df_features['Sub Section']==row.iloc[0]].iloc[0]['Feature'], 
+                     'Test Case'           : df_test_items[df_test_items['Section']==row.iloc[1]].iloc[0]['Title'],
                      'Verification Goal'   : df_test_items[df_test_items['Section']==row.iloc[1]].iloc[0]['Description'],
                      'Criteria Pass Fail'  : df_test_items[df_test_items['Section']==row.iloc[1]].iloc[0]['Criteria Pass Fail'],
-                     'Type'                : df_test_items[df_test_items['Section']==row.iloc[1]].iloc[0]['Type'],
+                     'Test Type'           : df_test_items[df_test_items['Section']==row.iloc[1]].iloc[0]['Test Type'],
+                     'Coverage Method'     : df_test_items[df_test_items['Section']==row.iloc[1]].iloc[0]['Coverage Method'],
+                     'link To Coverage'    : "",
                      })
+
 
 ## Create a Pandas Excel writer using XlsxWriter as the engine.
 writer = pd.ExcelWriter(args.output_xls, engine='xlsxwriter')
@@ -60,10 +64,26 @@ df = pd.DataFrame(top_dict)
 
 # remove cells which are duplicated
 # Shift gives the previous cell
-df['Feature']    = df['Feature'].mask(df['Feature'].eq(df['Feature'].shift()))
-df['Feature ID'] = df['Feature ID'].mask(df['Feature ID'].eq(df['Feature ID'].shift()))
-df['Sub Feature ID'] = df['Sub Feature ID'].mask(df['Sub Feature ID'].eq(df['Sub Feature ID'].shift()))
-df['Sub Feature'] = df['Sub Feature'].mask(df['Sub Feature'].eq(df['Sub Feature'].shift()))
+df['Feature']        = df['Feature'].mask(df['Feature'].eq(df['Feature'].shift()))
+df['Feature Description']        = df['Feature Description'].mask(df['Feature Description'].eq(df['Feature Description'].shift()))
+df['Requirement ID'] = df['Requirement ID'].mask(df['Requirement ID'].eq(df['Requirement ID'].shift()))
+#df['Sub Feature ID'] = df['Sub Feature ID'].mask(df['Sub Feature ID'].eq(df['Sub Feature ID'].shift()))
+df['Sub Feature']    = df['Sub Feature'].mask(df['Sub Feature'].eq(df['Sub Feature'].shift()))
+
+#for i, row in df_test_items.iterrows():
+#  if  row.iloc[1] not in df_mapping.values:
+#    if pd.isnull(row['Coverage Method']) == False:
+#        top_dict.append({'Requirement ID'      : 'NA',
+#                         'Feature'             : 'Verification Items', 
+#                         'Sub Feature'         : 'Verification Items', 
+#                         'Feature Description' : 'Verification Items', 
+#                         'Test Case'           : row['Title'],
+#                         'Verification Goal'   : row['Description'],
+#                         'Criteria Pass Fail'  : row['Criteria Pass Fail'],
+#                         'Test Type'           : row['Test Type'],
+#                         'Coverage Method'     : row['Coverage Method'],
+#                         })
+#df = pd.DataFrame(top_dict)
 
 # Convert the dataframe to an XlsxWriter Excel object. Note that we turn off
 # the default header and skip one row to allow us to insert a user defined
@@ -84,21 +104,23 @@ header_format = workbook.add_format(
         "border": 1,
     }
 )
+# format the columns
+col_1st    = writer.book.add_format({'bold':True, 'border': 1})
+col_others = writer.book.add_format({'bold':False, 'text_wrap': True, 'border': 1})
 
 # Write the column headers with the defined format.
 for col_num, value in enumerate(df.columns.values):
     worksheet.write(0, col_num, value, header_format)
 
-# format the columns
-col_1st    = writer.book.add_format({'bold':True, 'border': 1})
-col_others = writer.book.add_format({'bold':False, 'text_wrap': True, 'border': 1})
 
 #col_1st.set_bg_color('#CAF5FF')
 
-worksheet.set_column(0,7,100,col_others)
-worksheet.set_column(0,0,10,col_1st)
-worksheet.set_column(2,2,10,col_1st)
+worksheet.set_column(0,9,80,col_others)
+worksheet.set_column(0,0,20,col_1st)
+#worksheet.set_column(2,2,10,col_1st)
 
 # Close the Pandas Excel writer and output the Excel file.
+
+
 writer.close()
 
