1fe10f87b45b445322c4b378b7f6f80efd3867aa
mike <mike@openhwgroup.org>
2025-02-24 13:20:14 -0500

    Update RVFI agent to match version used by e40x

--- lib/uvm_agents/uvma_rvfi/uvma_rvfi_instr_mon.sv
+++ lib/uvm_agents/uvma_rvfi/uvma_rvfi_instr_mon.sv
@@ -29,22 +29,21 @@ class uvma_rvfi_instr_mon_c#(int ILEN=DEFAULT_ILEN,
                              int XLEN=DEFAULT_XLEN) extends uvm_monitor;
 
    // Objects
+   int unsigned       nret_id;
    uvma_rvfi_cfg_c    cfg;
    uvma_rvfi_cntxt_c  cntxt;
 
    // TLM
-   uvm_analysis_port#(uvma_rvfi_instr_seq_item_c#(ILEN,XLEN)) ap;
+   uvm_analysis_port#(uvma_rvfi_instr_seq_item_c#(ILEN,XLEN))  ap;
 
    // State of monitor
-   bit                last_dcsr_nmip = 0;
-   bit [uvma_rvfi_csr_seq_item_c#()::XLEN-1:0] dcsr_ret_data;
-
-   longint unsigned supported_csrs_addrs [$];
-   string supported_csrs_names [$];
+   bit            last_dcsr_nmip = 0;
+   bit [XLEN-1:0] dcsr_ret_data;
 
    string log_tag = "RVFIMONLOG";
 
    `uvm_component_utils_begin(uvma_rvfi_instr_mon_c)
+      `uvm_field_int(nret_id,        UVM_DEFAULT)
       `uvm_field_int(last_dcsr_nmip, UVM_DEFAULT)
       `uvm_field_object(cfg,         UVM_DEFAULT | UVM_REFERENCE)
       `uvm_field_object(cntxt,       UVM_DEFAULT | UVM_REFERENCE)
@@ -106,199 +105,141 @@ task uvma_rvfi_instr_mon_c::run_phase(uvm_phase phase);
 
    super.run_phase(phase);
 
-    cfg.core_cfg.get_supported_csrs_names(supported_csrs_names);
-    cfg.core_cfg.get_supported_csrs_addrs(supported_csrs_addrs);
+   if (cfg.enabled) begin
+      while (1) begin
+         wait (cntxt.instr_vif[nret_id].reset_n === 1'b0);
+         wait (cntxt.instr_vif[nret_id].reset_n === 1'b1);
 
-    // TODO Make this prettier
-    if (cfg.enabled) begin
-        while (1) begin
-            wait (cntxt.instr_vif[0].reset_n === 1'b0);
-            wait (cntxt.instr_vif[0].reset_n === 1'b1);
-
+         fork begin
             fork
-                begin
-                    monitor_rvfi_instr();
-                end
-                begin
-                    wait (cntxt.instr_vif[0].reset_n === 1'b0);
-                end
-            join_any
-        end
-    end
+               monitor_rvfi_instr();
+            join_none
+
+            wait (cntxt.instr_vif[nret_id].reset_n === 1'b0);
+
+            disable fork;
+         end
+         join
+      end
+   end
 endtask : run_phase
 
-
 task uvma_rvfi_instr_mon_c::monitor_rvfi_instr();
+   while(1) begin
+      @(cntxt.instr_vif[nret_id].mon_cb);
+      if (cntxt.instr_vif[nret_id].mon_cb.rvfi_valid) begin
+         uvma_rvfi_instr_seq_item_c#(ILEN,XLEN) mon_trn;
 
-  if (!cfg.ap_write_en) begin
-      `uvm_warning("RVFI INSTR MON", "Writing to Analysis Port has been disabled")
-  end
+         mon_trn = uvma_rvfi_instr_seq_item_c#(ILEN,XLEN)::type_id::create("rvfi_instr_mon_trn");
 
-  while(1) begin
-      @(cntxt.instr_vif[0].mon_cb)
+         mon_trn.nret_id  = nret_id;
+         mon_trn.cycle_cnt = cntxt.instr_vif[nret_id].mon_cb.cycle_cnt;
+         mon_trn.order     = cntxt.instr_vif[nret_id].mon_cb.rvfi_order;
+         mon_trn.insn      = cntxt.instr_vif[nret_id].mon_cb.rvfi_insn;
+         mon_trn.trap      = cntxt.instr_vif[nret_id].mon_cb.rvfi_trap;
+         mon_trn.halt      = cntxt.instr_vif[nret_id].mon_cb.rvfi_halt;
+         mon_trn.dbg       = cntxt.instr_vif[nret_id].mon_cb.rvfi_dbg;
+         mon_trn.dbg_mode  = cntxt.instr_vif[nret_id].mon_cb.rvfi_dbg_mode;
+         mon_trn.nmip      = cntxt.instr_vif[nret_id].mon_cb.rvfi_nmip;
+         mon_trn.intr      = cntxt.instr_vif[nret_id].mon_cb.rvfi_intr;
+         $cast(mon_trn.mode, cntxt.instr_vif[nret_id].mon_cb.rvfi_mode);
+         mon_trn.ixl       = cntxt.instr_vif[nret_id].mon_cb.rvfi_ixl;
+         mon_trn.pc_rdata  = cntxt.instr_vif[nret_id].mon_cb.rvfi_pc_rdata;
+         mon_trn.pc_wdata  = cntxt.instr_vif[nret_id].mon_cb.rvfi_pc_wdata;
 
-      for(int i = 0; i < cfg.nret; i++)
-      begin
-          int nret_id;
-          bit monitor_condition;
+         mon_trn.rs1_addr   = cntxt.instr_vif[nret_id].mon_cb.rvfi_rs1_addr;
+         mon_trn.rs1_rdata  = cntxt.instr_vif[nret_id].mon_cb.rvfi_rs1_rdata;
 
-          nret_id = i;
-          monitor_condition = (!cfg.core_cfg.unified_traps) ?
-                              (cntxt.instr_vif[nret_id].mon_cb.rvfi_valid || cntxt.instr_vif[nret_id].mon_cb.rvfi_trap[0]) :
-                              (cntxt.instr_vif[nret_id].mon_cb.rvfi_valid);
+         mon_trn.rs2_addr   = cntxt.instr_vif[nret_id].mon_cb.rvfi_rs2_addr;
+         mon_trn.rs2_rdata  = cntxt.instr_vif[nret_id].mon_cb.rvfi_rs2_rdata;
 
-          if (monitor_condition) begin
-              uvma_rvfi_instr_seq_item_c#(ILEN,XLEN) mon_trn;
+         mon_trn.rs3_addr   = cntxt.instr_vif[nret_id].mon_cb.rvfi_rs3_addr;
+         mon_trn.rs3_rdata  = cntxt.instr_vif[nret_id].mon_cb.rvfi_rs3_rdata;
 
-              mon_trn = uvma_rvfi_instr_seq_item_c#(ILEN,XLEN)::type_id::create("mon_trn");
+         mon_trn.rd1_addr   = cntxt.instr_vif[nret_id].mon_cb.rvfi_rd1_addr;
+         mon_trn.rd1_wdata  = cntxt.instr_vif[nret_id].mon_cb.rvfi_rd1_wdata;
 
-              mon_trn.nret_id  = nret_id;
-              mon_trn.cycle_cnt = cntxt.instr_vif[nret_id].mon_cb.cycle_cnt;
-              mon_trn.order     = cntxt.instr_vif[nret_id].mon_cb.rvfi_order;
-              mon_trn.insn      = cntxt.instr_vif[nret_id].mon_cb.rvfi_insn;
-              mon_trn.trap      = cntxt.instr_vif[nret_id].mon_cb.rvfi_trap;
-              mon_trn.halt      = cntxt.instr_vif[nret_id].mon_cb.rvfi_halt;
-              mon_trn.dbg       = cntxt.instr_vif[nret_id].mon_cb.rvfi_dbg;
-              mon_trn.dbg_mode  = cntxt.instr_vif[nret_id].mon_cb.rvfi_dbg_mode;
-              mon_trn.nmip      = cntxt.instr_vif[nret_id].mon_cb.rvfi_nmip;
-              mon_trn.intr      = cntxt.instr_vif[nret_id].mon_cb.rvfi_intr;
-              $cast(mon_trn.mode, cntxt.instr_vif[nret_id].mon_cb.rvfi_mode);
-              mon_trn.ixl       = cntxt.instr_vif[nret_id].mon_cb.rvfi_ixl;
-              mon_trn.pc_rdata  = cntxt.instr_vif[nret_id].mon_cb.rvfi_pc_rdata;
-              mon_trn.pc_wdata  = cntxt.instr_vif[nret_id].mon_cb.rvfi_pc_wdata;
+         mon_trn.rd2_addr   = cntxt.instr_vif[nret_id].mon_cb.rvfi_rd2_addr;
+         mon_trn.rd2_wdata  = cntxt.instr_vif[nret_id].mon_cb.rvfi_rd2_wdata;
 
-              mon_trn.rs1_addr   = cntxt.instr_vif[nret_id].mon_cb.rvfi_rs1_addr;
-              mon_trn.rs1_rdata  = cntxt.instr_vif[nret_id].mon_cb.rvfi_rs1_rdata;
+         mon_trn.mem_addr  = cntxt.instr_vif[nret_id].mon_cb.rvfi_mem_addr;
+         mon_trn.mem_rdata = cntxt.instr_vif[nret_id].mon_cb.rvfi_mem_rdata;
+         mon_trn.mem_rmask = cntxt.instr_vif[nret_id].mon_cb.rvfi_mem_rmask;
+         mon_trn.mem_wdata = cntxt.instr_vif[nret_id].mon_cb.rvfi_mem_wdata;
+         mon_trn.mem_wmask = cntxt.instr_vif[nret_id].mon_cb.rvfi_mem_wmask;
 
-              mon_trn.rs2_addr   = cntxt.instr_vif[nret_id].mon_cb.rvfi_rs2_addr;
-              mon_trn.rs2_rdata  = cntxt.instr_vif[nret_id].mon_cb.rvfi_rs2_rdata;
+         mon_trn.gpr_rdata = cntxt.instr_vif[nret_id].mon_cb.rvfi_gpr_rdata;
+         mon_trn.gpr_rmask = cntxt.instr_vif[nret_id].mon_cb.rvfi_gpr_rmask;
+         mon_trn.gpr_wdata = cntxt.instr_vif[nret_id].mon_cb.rvfi_gpr_wdata;
+         mon_trn.gpr_wmask = cntxt.instr_vif[nret_id].mon_cb.rvfi_gpr_wmask;
 
-              mon_trn.rs3_addr   = cntxt.instr_vif[nret_id].mon_cb.rvfi_rs3_addr;
-              mon_trn.rs3_rdata  = cntxt.instr_vif[nret_id].mon_cb.rvfi_rs3_rdata;
+         // Get the CSRs
+         foreach (cntxt.csr_vif[csr]) begin
+            uvma_rvfi_csr_seq_item_c csr_trn = uvma_rvfi_csr_seq_item_c#(XLEN)::type_id::create({csr, "_trn"});
 
-              mon_trn.rd1_addr   = cntxt.instr_vif[nret_id].mon_cb.rvfi_rd1_addr;
-              mon_trn.rd1_wdata  = cntxt.instr_vif[nret_id].mon_cb.rvfi_rd1_wdata;
+            csr_trn.csr = csr;
+            csr_trn.nret_id = nret_id;
+            csr_trn.rmask = cntxt.csr_vif[csr][nret_id].mon_cb.rvfi_csr_rmask;
+            csr_trn.wmask = cntxt.csr_vif[csr][nret_id].mon_cb.rvfi_csr_wmask;
+            csr_trn.rdata = cntxt.csr_vif[csr][nret_id].mon_cb.rvfi_csr_rdata;
+            csr_trn.wdata = cntxt.csr_vif[csr][nret_id].mon_cb.rvfi_csr_wdata;
 
-              mon_trn.rd2_addr   = cntxt.instr_vif[nret_id].mon_cb.rvfi_rd2_addr;
-              mon_trn.rd2_wdata  = cntxt.instr_vif[nret_id].mon_cb.rvfi_rd2_wdata;
+            mon_trn.csrs[csr] = csr_trn;
+         end
 
-              mon_trn.mem_addr  = cntxt.instr_vif[nret_id].mon_cb.rvfi_mem_addr;
-              mon_trn.mem_rdata = cntxt.instr_vif[nret_id].mon_cb.rvfi_mem_rdata;
-              mon_trn.mem_rmask = cntxt.instr_vif[nret_id].mon_cb.rvfi_mem_rmask;
-              mon_trn.mem_wdata = cntxt.instr_vif[nret_id].mon_cb.rvfi_mem_wdata;
-              mon_trn.mem_wmask = cntxt.instr_vif[nret_id].mon_cb.rvfi_mem_wmask;
+         //TODO:MT update with parity for 40S
+         mon_trn.insn_nmi = 0;
+         if (mon_trn.intr.intr) begin
+            // NMI detected
+            if ((cfg.nmi_load_fault_enabled && mon_trn.intr.cause == cfg.nmi_load_fault_cause) ||
+               (cfg.nmi_store_fault_enabled && mon_trn.intr.cause == cfg.nmi_store_fault_cause)) begin
+               mon_trn.insn_nmi = 1;
+               mon_trn.insn_nmi_cause = mon_trn.intr.cause;
+            end
+            // External interrupt
+            else begin
+               mon_trn.insn_interrupt    = 1;
+               mon_trn.insn_interrupt_id = { 1'b0, mon_trn.intr.cause };
+            end
+         end
 
-              // Get the CSRs
-              if (!cfg.core_cfg.disable_all_csr_checks) begin
-                  if (!cfg.unified_csr_vif) begin
-                      foreach (cntxt.csr_vif[csr]) begin
-                          uvma_rvfi_csr_seq_item_c csr_trn = uvma_rvfi_csr_seq_item_c#(XLEN)::type_id::create({csr, "_trn"});
+         dcsr_ret_data = mon_trn.csrs["dcsr"].get_csr_retirement_data();
+         // In debug mode, detect NMIP event for a data bus error
+         if (mon_trn.dbg_mode &&
+             !last_dcsr_nmip &&
+             mon_trn.nmip[0] &&
+             dcsr_ret_data[3])
+         begin
+            `uvm_info("RVFIMON", $sformatf("Debug NMIP"), UVM_LOW)
 
-                          csr_trn.csr = csr;
-                          csr_trn.nret_id = nret_id;
-                          csr_trn.rmask = cntxt.csr_vif[csr][nret_id].mon_cb.rvfi_csr_rmask;
-                          csr_trn.wmask = cntxt.csr_vif[csr][nret_id].mon_cb.rvfi_csr_wmask;
-                          csr_trn.rdata = cntxt.csr_vif[csr][nret_id].mon_cb.rvfi_csr_rdata;
-                          csr_trn.wdata = cntxt.csr_vif[csr][nret_id].mon_cb.rvfi_csr_wdata;
+            if (mon_trn.nmip[1] == 0) begin
+               mon_trn.insn_nmi = 1;
+               mon_trn.insn_nmi_cause = cfg.nmi_load_fault_cause;
+            end else begin
+               mon_trn.insn_nmi = 1;
+               mon_trn.insn_nmi_cause = cfg.nmi_store_fault_cause;
+            end
+         end
 
-                          mon_trn.name_csrs[csr] = csr_trn;
-                      end
-                  end
-                  else begin
-                      uvma_rvfi_csr_seq_item_c csr_trn;
-                      longint unsigned addr;
+         // Detect instruction bus fault
+         if (cfg.insn_bus_fault_enabled &&
+             mon_trn.is_trap() &&
+             mon_trn.get_trap_cause() == cfg.insn_bus_fault_cause) begin
+            mon_trn.insn_bus_fault = 1;
+            `uvm_info("RVFIMON", $sformatf("Detected bus fault"), UVM_LOW)
+         end
 
-                      csr_trn = uvma_rvfi_csr_seq_item_c#(XLEN)::type_id::create({addr, "_trn"});
-                      addr = cntxt.csr_unified_vif[nret_id].mon_cb.rvfi_csr_addr;
-                      csr_trn.addr = addr;
-                      if (cntxt.csr_unified_vif[nret_id].mon_cb.rvfi_csr_wmask != 0) begin
-                          csr_trn.wmask = cntxt.csr_unified_vif[nret_id].mon_cb.rvfi_csr_wmask;
-                          csr_trn.wdata = cntxt.csr_unified_vif[nret_id].mon_cb.rvfi_csr_wdata;
-                          csr_trn.rmask = cntxt.csr_unified_vif[nret_id].mon_cb.rvfi_csr_rmask;
-                          csr_trn.rdata = cntxt.csr_unified_vif[nret_id].mon_cb.rvfi_csr_rdata;
-                          mon_trn.addr_csrs[addr] = csr_trn;
-                      end
-                      foreach(supported_csrs_addrs[i]) begin
-                          addr = supported_csrs_addrs[i];
-                          csr_trn = uvma_rvfi_csr_seq_item_c#(XLEN)::type_id::create({addr, "_trn"});
-                          csr_trn.addr = addr;
-                          csr_trn.wmask = cntxt.csr_unified_vif[nret_id].mon_cb.rvfi_named_csr_wmask[addr];
-                          csr_trn.wdata = cntxt.csr_unified_vif[nret_id].mon_cb.rvfi_named_csr_wdata[addr];
-                          csr_trn.rmask = cntxt.csr_unified_vif[nret_id].mon_cb.rvfi_named_csr_rmask[addr];
-                          csr_trn.rdata = cntxt.csr_unified_vif[nret_id].mon_cb.rvfi_named_csr_rdata[addr];
-                          mon_trn.addr_csrs[addr] = csr_trn;
-                      end
-                  end
+         // Latch the last DCSR NMIP bit to detect positive assertion of dcsr.nmip
+         last_dcsr_nmip = dcsr_ret_data[3];
 
-                  // Decode interrupts that need to be communicated to ISS (external or NMI bus faults)
-                  if (mon_trn.intr) begin
-                      // The cause of the interrupt should be in the "rdata" field of the mcause CSR RVFI port
-                      bit [XLEN-1:0] csr_mcause;
+         `uvm_info(log_tag, $sformatf("%s", mon_trn.convert2string()), UVM_HIGH);
 
-                      if (!cfg.unified_csr_vif)
-                          csr_mcause = mon_trn.name_csrs["mcause"].rdata;
-                      else
-                          csr_mcause = mon_trn.addr_csrs[MCAUSE].rdata;
-
-                      // External interrupt
-                      if (csr_mcause[31]) begin
-                      // NMI - Load fault
-                          if (cfg.nmi_load_fault_enabled && csr_mcause[XLEN-2:0] == cfg.nmi_load_fault_cause) begin
-                              mon_trn.insn_nmi_load_fault = 1;
-                          end
-                          // NMI - Store fault
-                          else if (cfg.nmi_store_fault_enabled && csr_mcause[XLEN-2:0] == cfg.nmi_store_fault_cause) begin
-                              mon_trn.insn_nmi_store_fault = 1;
-                          end
-                          // External interrupt
-                          else begin
-                              mon_trn.insn_interrupt    = 1;
-                              mon_trn.insn_interrupt_id = { 1'b0, csr_mcause[XLEN-2:0] };
-                          end
-                      end
-                  end
-                  if (!cfg.unified_csr_vif && mon_trn.name_csrs.exists("dcsr"))
-                      dcsr_ret_data = mon_trn.name_csrs["dcsr"].get_csr_retirement_data();
-                  else if (cfg.unified_csr_vif && mon_trn.addr_csrs.exists(DCSR))
-                      dcsr_ret_data = mon_trn.addr_csrs[DCSR].get_csr_retirement_data();
-
-                  // In debug mode, detect NMIP event for a data bus error
-                  if (mon_trn.dbg_mode &&
-                      !last_dcsr_nmip &&
-                      mon_trn.nmip[0] &&
-                      dcsr_ret_data[3])
-                  begin
-                      `uvm_info("RVFIMON", $sformatf("Debug NMIP"), UVM_LOW);
-
-                      if (mon_trn.nmip[1] == 0) begin
-                      mon_trn.insn_nmi_load_fault = 1;
-                      end else begin
-                      mon_trn.insn_nmi_store_fault = 1;
-                      end
-                  end
-
-                  // Latch the last DCSR NMIP bit to detect positive assertion of dcsr.nmip
-                  last_dcsr_nmip = dcsr_ret_data[3];
-              end
-
-              // Detect instruction bus fault
-              if (cfg.insn_bus_fault_enabled &&
-                  mon_trn.is_trap() &&
-                  mon_trn.get_trap_cause() == cfg.insn_bus_fault_cause) begin
-                  mon_trn.insn_bus_fault = 1;
-                  `uvm_info("RVFIMON", $sformatf("Detected bus fault"), UVM_LOW)
-              end
-
-              `uvm_info(log_tag, $sformatf("%s", mon_trn.convert2string()), UVM_HIGH);
-
-              if (cfg.ap_write_en) begin
-                  ap.write(mon_trn);
-              end
-          end
+         ap.write(mon_trn);
       end
-  end
+   end
 
 endtask : monitor_rvfi_instr
 
 `endif // __UVMA_RVFI_INSTR_MON_SV__
+
 
--- lib/uvm_agents/uvma_rvfi/uvma_rvfi_tdefs.sv
+++ lib/uvm_agents/uvma_rvfi/uvma_rvfi_tdefs.sv
@@ -26,66 +26,20 @@ typedef enum bit[MODE_WL-1:0] {
 } uvma_rvfi_mode;
 
 typedef struct packed {
-   bit [MAX_XLEN-1:0]         nret_id;
-   bit [MAX_XLEN-1:0]         cycle_cnt;
-   bit [MAX_XLEN-1:0]         order;
-   bit [MAX_XLEN-1:0]         insn;
-   bit [MAX_XLEN-1:0]         trap;
-   bit [MAX_XLEN-1:0]         halt;
-   bit [MAX_XLEN-1:0]         intr;
-   uvma_rvfi_mode             mode;
-   bit [MAX_XLEN-1:0]         ixl;
-   bit [MAX_XLEN-1:0]         dbg;
-   bit [MAX_XLEN-1:0]         dbg_mode;
-   bit [MAX_XLEN-1:0]         nmip;
+  logic [10:0] cause;
+  logic        interrupt;
+  logic        exception;
+  logic        intr;
+} rvfi_intr_t;
 
-   bit [MAX_XLEN-1:0]         insn_interrupt;
-   bit [MAX_XLEN-1:0]         insn_interrupt_id;
-   bit [MAX_XLEN-1:0]         insn_bus_fault;
-   bit [MAX_XLEN-1:0]         insn_nmi_store_fault;
-   bit [MAX_XLEN-1:0]         insn_nmi_load_fault;
-
-   bit [MAX_XLEN-1:0]         pc_rdata;
-   bit [MAX_XLEN-1:0]         pc_wdata;
-
-   bit [MAX_XLEN-1:0]         rs1_addr;
-   bit [MAX_XLEN-1:0]         rs1_rdata;
-
-   bit [MAX_XLEN-1:0]         rs2_addr;
-   bit [MAX_XLEN-1:0]         rs2_rdata;
-
-   bit [MAX_XLEN-1:0]         rs3_addr;
-   bit [MAX_XLEN-1:0]         rs3_rdata;
-
-   bit [MAX_XLEN-1:0]         rd1_addr;
-   bit [MAX_XLEN-1:0]         rd1_wdata;
-
-   bit [MAX_XLEN-1:0]         rd2_addr;
-   bit [MAX_XLEN-1:0]         rd2_wdata;
-
-   bit [MAX_XLEN-1:0]         mem_addr;
-   bit [MAX_XLEN-1:0]         mem_rdata;
-   bit [MAX_XLEN-1:0]         mem_rmask;
-   bit [MAX_XLEN-1:0]         mem_wdata;
-   bit [MAX_XLEN-1:0]         mem_wmask;
-
-   bit [CSR_MAX_SIZE-1:0] [MAX_XLEN-1:0] csr_valid;
-   bit [CSR_MAX_SIZE-1:0] [MAX_XLEN-1:0] csr_addr;
-   bit [CSR_MAX_SIZE-1:0] [MAX_XLEN-1:0] csr_rdata;
-   bit [CSR_MAX_SIZE-1:0] [MAX_XLEN-1:0] csr_rmask;
-   bit [CSR_MAX_SIZE-1:0] [MAX_XLEN-1:0] csr_wdata;
-   bit [CSR_MAX_SIZE-1:0] [MAX_XLEN-1:0] csr_wmask;
-
-} st_rvfi;
-
-`define ST_NUM_WORDS ($size(st_rvfi)/MAX_XLEN)
-parameter ST_NUM_WORDS =  ($size(st_rvfi)/MAX_XLEN);
-
-    typedef bit [ST_NUM_WORDS-1:0] [MAX_XLEN-1:0] vector_rvfi;
-typedef union {
-    st_rvfi rvfi;
-    vector_rvfi array;
-} union_rvfi;
+typedef struct packed {
+  logic [1:0]  cause_type;
+  logic [2:0]  debug_cause;
+  logic [5:0]  exception_cause;
+  logic        debug;
+  logic        exception;
+  logic        trap;
+} rvfi_trap_t;
 
 function string get_mode_str(uvma_rvfi_mode mode);
    case (mode)
@@ -97,13 +51,5 @@ function string get_mode_str(uvma_rvfi_mode mode);
    return "?";
 
 endfunction : get_mode_str
-
-function bit is_csr_insn (st_rvfi rvfi);
-    return (rvfi.insn[6:0] == 'b1110011);
-endfunction : is_csr_insn
-
-function bit [11:0] get_insn_rs1 (st_rvfi rvfi);
-    return rvfi.insn[31:20];
-endfunction
 
 `endif // __UVMA_RVFI_TDEFS_SV__
