699f8e5f17b8b44f15e9891327b07a3a8a1259ff
Tan Siret A. <tansiretakinci@protonmail.com>
2026-02-06 11:52:26 +0100

    make header fit contributing guide

--- cv32e40p/tests/programs/custom/mscratch_rw/mscratch_rw.c
+++ cv32e40p/tests/programs/custom/mscratch_rw/mscratch_rw.c
@@ -1,25 +1,8 @@
-/*
-**
-** Copyright 2026 OpenHW Group
-**
-** Licensed under the Solderpad Hardware Licence, Version 2.0 (the "License");
-** you may not use this file except in compliance with the License.
-** You may obtain a copy of the License at
-**
-**     https://solderpad.org/licenses/
-**
-** Unless required by applicable law or agreed to in writing, software
-** distributed under the License is distributed on an "AS IS" BASIS,
-** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-** See the License for the specific language governing permissions and
-** limitations under the License.
-**
-*******************************************************************************
-**
-** Read/Write access test to all 32-bits of the mscratch CSR.
-**
-*******************************************************************************
-*/
+// Copyright (c) 2026 OpenHW Group
+// SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
+//
+// Read/Write access test to all 32-bits of the mscratch CSR.
+//
 
 #include <stdio.h>
 #include <stdlib.h>
